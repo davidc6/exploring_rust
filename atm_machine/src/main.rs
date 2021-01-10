ae492ca9a60e846c599d30cbc762d3f6ae31830b
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2021-01-10 19:29:53 +0000

    Add menu options

--- atm_machine/src/main.rs
+++ atm_machine/src/main.rs
@@ -17,12 +17,70 @@ fn is_valid_pin(user_input: String) -> bool {
     }
 }
 
+fn deposit(balance: &mut u32) {
+    println!("How much do you want to deposit? (£)");
+
+    let mut user_input = String::new();
+
+    stdin().read_line(&mut user_input).ok();
+
+    match user_input.trim().parse::<u32>() {
+        Ok(input) => {
+            if input > 0 {
+                *balance += input;
+                println!("£{} has been deposited.", input);
+            }            
+        },
+        Err(_) => println!("Please deposit a valid amount.")
+    }
+}
+
+fn withdraw(balance: &mut u32) {
+    println!("How much do you want to withdraw? (£)");
+
+    let mut user_input = String::new();
+
+    stdin().read_line(&mut user_input).ok();
+    
+    match user_input.trim().parse::<u32>() {
+        Ok(input) => {
+            if *balance >= input {
+                *balance -= input;
+                println!("£{} has been withdrawn.", input);
+                return;
+            }
+            println!("Your balance is less than the amount that want to withdraw.");
+        },
+        Err(_) => println!("Please withdraw a valid amount.")
+    }
+}
+
 fn menu() {
-  println!("Options:");
-  println!("1 - Balance");
-  println!("2 - Deposit");
-  println!("3 - Withdraw");
-  println!("4 - Quit");
+    let mut should_quit = false;
+    let mut balance: u32 = 0;
+
+    while !should_quit {
+        println!("Options:");
+        println!("1 - Balance");
+        println!("2 - Deposit");
+        println!("3 - Withdraw");
+        println!("4 - Quit");
+
+        // stored as vector of bytes
+        let mut user_input = String::new();
+
+        stdin().read_line(&mut user_input).ok();
+
+        user_input = String::from(user_input.trim());
+
+        match user_input.as_str() {
+            "1" => println!("Your balance is: £{}.", balance),
+            "2" => deposit(&mut balance),
+            "3" => withdraw(&mut balance),
+            "4" => should_quit = true,
+            _ => println!("Option not recognised")
+        }
+    }
 }
 
 fn main() {
@@ -31,9 +89,9 @@ fn main() {
     let mut user_input = String::new();
 
     match stdin().read_line(&mut user_input) {
-        Ok(_) => {            
+        Ok(_) => {
             if is_valid_pin(user_input) {
-                println!("Valid pin");
+                println!("Welcome!");
                 menu();
             } else {
                 println!("Invalid pin");
@@ -41,6 +99,6 @@ fn main() {
         }
         Err(error) => println!("error: {}", error)
     }
-    
+
     println!("Goodbye.");
 }
