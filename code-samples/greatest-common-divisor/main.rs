47c2613c6fadc2ebfbaf7e949d993867680137a6
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-04-09 21:17:03 +0000

    Update comments

--- code-samples/greatest-common-divisor/main.rs
+++ code-samples/greatest-common-divisor/main.rs
@@ -4,11 +4,11 @@ fn main() {
   // read from command line and save to a vector
   let args: Vec<String>  = env::args().collect();
   
-  let my_string = &args[1].to_string(); // dereference to the first command line variable to string
+  let my_string = &args[1].to_string(); // convert first cli arg to string
   let mut n: i32 = my_string.parse().unwrap_or(0);
 
   let my_string_2 = &args[2].to_string();
-  let mut m: i32 = my_string_2.parse().unwrap_or(0); // reference to the second command line variable
+  let mut m: i32 = my_string_2.parse().unwrap_or(0); // convert second cli arg to string
   
   assert!(n != 0 && m != 0); // 
   
