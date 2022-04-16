eb9204bc2b8413692798d1ac2402f2ee7a8d749c
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-04-16 09:43:01 +0000

    Improve gcd

--- cargo-projects/greatest-common-divisor/src/main.rs
+++ cargo-projects/greatest-common-divisor/src/main.rs
@@ -1,17 +1,19 @@
 use std::env;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+  let mut args: Vec<String> = env::args().collect();
   
   if args.len() == 1 {
-    println!("Usage: gcd 10 90");
+    eprintln!("Usage: gcd <number> <number>");
     return;
   }
   
-  let first = &args[1].to_string();
+  args.remove(0);
+
+  let first = &args[0].to_string();
   let first_number: u64 = first.parse().unwrap_or(0);
   
-  let second = &args[2].to_string();
+  let second = &args[1].to_string();
   let second_number: u64 = second.parse().unwrap_or(0);
   
   let number = greatest_common_divisor(first_number, second_number);
@@ -21,7 +23,7 @@ fn main() {
 
 fn greatest_common_divisor(mut n: u64, mut m: u64) -> u64 {
   while m != 0 {
-    if m < n {
+    if m < n { 
       let t = m;
       m = n;
       n = t;
@@ -31,9 +33,18 @@ fn greatest_common_divisor(mut n: u64, mut m: u64) -> u64 {
   return n;
 }
 
-#[test]
-fn test_gcd() {
-  assert_eq!(greatest_common_divisor(10, 90), 10);
+#[cfg(test)]
+mod tests {
+  // import names from the outer scope
+  use super::*;
+
+  #[test]
+  fn test_gcd_1() {
+    assert_eq!(greatest_common_divisor(10, 90), 10);    
+  }
   
-  assert_eq!(greatest_common_divisor(1, 2), 1);
+  #[test]
+  fn test_gcd_2() {
+    assert_eq!(greatest_common_divisor(1, 2), 1);    
+  }
 }
