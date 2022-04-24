fbe769c2a24da8e1c70dd28cb6ee76ed052da148
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-04-24 16:31:04 +0000

    Update integration tests

--- cargo-projects/greatest-common-divisor/tests/cli.rs
+++ cargo-projects/greatest-common-divisor/tests/cli.rs
@@ -1,7 +1,22 @@
 use assert_cmd::Command;
+use predicates::prelude::*;
 
 #[test]
-fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+fn runs_single_arg() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("greatest-common-divisor")?;
+  
+  cmd
+    .args(&["90"]);
+  cmd
+    .assert()
+    .success()
+    .stdout("90\n");
+    
+  Ok(())
+}
+
+#[test]
+fn runs_two_args() -> Result<(), Box<dyn std::error::Error>> {
   let mut cmd = Command::cargo_bin("greatest-common-divisor")?;
   
   cmd
@@ -10,6 +25,18 @@ fn it_works() -> Result<(), Box<dyn std::error::Error>> {
     .assert()
     .success()
     .stdout("10\n");
+    
+  Ok(())
+}
+
+#[test]
+fn errors_no_args() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("greatest-common-divisor")?;
+
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("No arguments passed in"));
     
   Ok(())
 }
