5843f892281b512832396fb1ad8094622eace2f5
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-05-03 22:14:01 +0100

    Enable mod access to sruct

--- code-samples/struct-impl-trait/main.rs
+++ code-samples/struct-impl-trait/main.rs
@@ -3,7 +3,7 @@ use std::fmt;
 // we add outer attribute to opt in to be able to debug
 // since structs do not enable this by default
 #[derive(Debug)]
-struct Playable {
+pub struct Playable {
   id: String,
   title: String,
   bitrate: u8,
@@ -23,10 +23,10 @@ impl Playable {
     }
     return false
   }
-  
+
   fn is_next(&self, other_item: &Playable) -> bool {
     let diff = other_item.order - self.order;
-    
+
     if diff == 1 {
       return true;
     }
@@ -48,25 +48,46 @@ impl IdTitle for Playable {
 //   }
 // }
 
+// modules
+mod item_formatter {
+  pub mod displaying {
+    pub fn show(id: &str, title: &str) -> String {
+      return format!("id: {}, title: {}", &id, &title)
+    }
+  }
+
+  pub mod editing {
+    pub fn change_name(playable: &mut crate::Playable) {
+      // mutable reference allows to mutate title
+      playable.title = String::from("Hello World");
+    }
+  }
+}
+
 fn main() {
-  let playableOne = Playable {
+  let mut playable_one = Playable {
     id: String::from("a1"),
     title: String::from("Title 1"),
     bitrate: 132,
     order: 1
   };
-  
-  let playableTwo = Playable {
+
+  let playable_two = Playable {
     id: String::from("a2"),
     title: String::from("Title 2"),
     bitrate: 132,
     order: 3
   };
-  
+
   // There's no -> or . (as in C/C++)
   // Instead there's automatic referencing and dereferencing
   // &, &mut, or * automatically get added
   // it is the same as (&playableOne).is_next(&playableTwo)
-  println!("{:?}", playableOne.is_next(&playableTwo));
-  println!("{:?}", playableOne.id_title());
+  println!("{:?}", playable_one.is_next(&playable_two));
+  println!("{:?}", playable_one.id_title());
+  println!("{:?}", playable_one.is_low_bitrate());
+  println!("{:?}", item_formatter::displaying::show(&playable_one.id, &playable_one.title));
+  // pass mutable reference (address that can be followed)
+  item_formatter::editing::change_name(&mut playable_one);
+  println!("{:?}", playable_one.id_title());
 }
