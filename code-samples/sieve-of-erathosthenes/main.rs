79514efe49095fe0700a5a1f552d46b9581e0b1e
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-05-09 07:45:39 +0100

    Add a test to sieve project

--- code-samples/sieve-of-erathosthenes/main.rs
+++ code-samples/sieve-of-erathosthenes/main.rs
@@ -27,10 +27,26 @@ fn sieve<const N: usize>() -> [bool; N] {
 fn main() {
   const X: usize = 7;
   let array = sieve::<{X + 1}>();
-
+    
   for (i, &val) in array.iter().enumerate() {
     if &val == &true && i > 2 {
       println!("{:?}", i);
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  // since test is an inner module and out code is in the outer module,
+  // we need to bring the outer module into the scope
+  use super::*;
+
+  #[test]
+  fn it_works() {
+    const N: usize = 5;
+    let arr = [true, true, true, true, false];
+    let res = sieve::<{ N }>();
+
+    assert_eq!(arr, res);
   }
 }
