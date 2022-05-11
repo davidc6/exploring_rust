c560a7f3fefa0d1275b2c48f3dd0b86b2f38984e
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-05-11 20:07:01 +0000

    Clean up

--- cargo-projects/litegrep/src/main.rs
+++ cargo-projects/litegrep/src/main.rs
@@ -4,18 +4,19 @@ use std::process;
 use litegrep::Configuration;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  // let config = parse_args(&args);
-  let config = Configuration::new(&args)
-    .unwrap_or_else(|err| {
+    let args: Vec<String> = env::args().collect();
+    // let config = parse_args(&args);
+    let config = Configuration::new(&args).unwrap_or_else(|err| {
       println!("Problem with arguments: {}", err);
       process::exit(1);
     });
-
-  // Result<String> gets returned
-  // expect() unwraps and allow to provide custom panic message
-  if let Err(e) = litegrep::execute(config) {
-    println!("Error: {}", e);
-    process::exit(1);
-  }
+    
+    println!()
+    
+    // Result<String> gets returned
+    // expect() unwraps and allow to provide custom panic message
+    if let Err(e) = litegrep::execute(config) {
+      println!("Error: {}", e);
+      process::exit(1);
+    }
 }
