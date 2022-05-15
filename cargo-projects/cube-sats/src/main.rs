4b7c27c5ece96d58d35b69221fe4ea3d4b105e70
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2022-05-15 10:07:11 +0000

    Refactor

--- cargo-projects/cube-sats/src/main.rs
+++ cargo-projects/cube-sats/src/main.rs
@@ -6,43 +6,85 @@ struct CubeSat {
   messages: Vec<String>
 }
 
+struct Message {
+  content: String,
+  to: u64
+}
+
 impl CubeSat {
-  fn receive(&mut self) -> Option<String> {
-    self.messages.pop()
+  fn receive(&self, mailbox: &mut Mailbox) -> Option<Message> {
+    mailbox.deliver(&self)
   }
 }
 
-struct GroundBase {
-  id: u16
-}
-
-impl GroundBase {
-  fn send_msg(&self, sat: &mut CubeSat, message: String) {
-    sat.messages.push(message);
-  }
-}
+struct GroundBase;
 
 impl GroundBase {
   fn connect(&self, sat_id: u64) -> CubeSat {
     CubeSat { id: sat_id, messages: vec![] }
   }
+
+  fn send(&self, to: &mut CubeSat, msg: String) {
+    to.messages.push(msg);
+  }
+}
+
+struct Mailbox {
+  // to: u64,
+  messages: Vec<Message>
+}
+
+impl Mailbox {
+  fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+    for i in 0..self.messages.len() {
+      if self.messages[i].to == recipient.id {
+        let msg = self.messages.remove(i);
+        return Some(msg);
+      }
+    }
+    None
+  }
+}
+
+fn fetch_all_sat_ids() -> Vec<u64> {
+  vec![1,2,3]
 }
 
 fn main() {
-  let ground_base = GroundBase { id: 1 };
-
-  let mut sat_a = CubeSat { id: 1, messages: Vec::new() };
-  let message_a = String::from("hello"); // stored on the heap
+  let mut mail = Mailbox { messages: vec![] };
+  let ground_base = GroundBase{};
   
-  let mut sat_b = CubeSat { id: 2, messages: Vec::new() };
-  let message_b = String::from("hello 2");
+  let sat_ids = fetch_all_sat_ids();
   
-  ground_base.send_msg(&mut sat_a, message_a);
-  ground_base.send_msg(&mut sat_b, message_b);
+  for id in sat_ids {
+    let mut sat = ground_base.connect(id);
+    let msg = String::from("hello");
 
-  // println!("{:?}", sat_a);
-  println!("{}", ground_base.id);
-  println!("CubeSat ID: {}, message: {:?}", sat_a.id, sat_a.messages);
-  println!("{}", sat_a.receive().unwrap());
-  println!("CubeSat ID: {}, message: {:?}", sat_b.id, sat_b.messages);
+    ground_base.send(&mut sat, msg);
+
+    println!("CubeSat ID: {}, message: {:?}", sat.id, sat.messages);
+  }
+  
+  let sat_ids = fetch_all_sat_ids();
+  
+  for id in sat_ids {
+    let sat = ground_base.connect(id);
+    
+    let msg = sat.receive(&mut mail);
+  }
+  
+  // let mut sat_a = CubeSat { id: 1, messages: Vec::new() };
+  // let message_a = String::from("hello"); // stored on the heap
+  
+  // let mut sat_b = CubeSat { id: 2, messages: Vec::new() };
+  // let message_b = String::from("hello 2");
+  
+  // ground_base.send_msg(&mut sat_a, message_a);
+  // ground_base.send_msg(&mut sat_b, message_b);
+
+  // // println!("{:?}", sat_a);
+  // println!("{}", ground_base.id);
+  // println!("CubeSat ID: {}, message: {:?}", sat_a.id, sat_a.messages);
+  // println!("{}", sat_a.receive().unwrap());
+  // println!("CubeSat ID: {}, message: {:?}", sat_b.id, sat_b.messages);
 }
