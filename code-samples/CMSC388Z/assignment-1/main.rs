2762a22c4b5ced319980df0fd0c3fd514f2d1b5f
David Chubabriya <david.chubabriya@gmail.com>
2022-05-28 19:04:51 +0100

    Add double funcions

--- code-samples/CMSC388Z/assignment-1/main.rs
+++ code-samples/CMSC388Z/assignment-1/main.rs
@@ -1,9 +1,29 @@
 fn main() {
+  let num = double(&2);
+  println!("{}", num);
+
+  let mut number = 2;
+  double_in_place(&mut number);
+  println!("{:?}", number);
+
+  let num = double_to_i64(&2);
+  println!("{}", num);
+
   let tup = (1,5);
   let slice = slice_arr(&[1,2,3,4], tup);
   println!("{:?}", slice);
+}
 
+fn double(x: &i32) -> i32 {
+  return (*x * 2) as i32;
+}
 
+fn double_in_place(x: &mut i32) {
+  *x *= 2
+}
+
+fn double_to_i64(x: &i32) -> i64 {
+  return (x * 2) as i64;
 }
 
 // Slice an array if in range
