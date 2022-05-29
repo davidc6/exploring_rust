59d22b89788b093791ebed3cf8e94e35ff787b8f
David Chubabriya <david.chubabriya@gmail.com>
2022-05-29 22:11:56 +0100

    Add square root of int algo

--- code-samples/CMSC388Z/assignment-1/main.rs
+++ code-samples/CMSC388Z/assignment-1/main.rs
@@ -1,17 +1,20 @@
 fn main() {
   let num = double(&2);
-  println!("{}", num);
+  println!("double: {}", num);
 
   let mut number = 2;
   double_in_place(&mut number);
-  println!("{:?}", number);
+  println!("double in place: {:?}", number);
 
   let num = double_to_i64(&2);
-  println!("{}", num);
+  println!("double i32 to i64: {}", num);
 
   let tup = (1,5);
   let slice = slice_arr(&[1,2,3,4], tup);
-  println!("{:?}", slice);
+  println!("slice: {:?}", slice);
+
+  let res = sqrt(124);
+  println!("square root: {}", res);
 }
 
 fn double(x: &i32) -> i32 {
@@ -26,11 +29,26 @@ fn double_to_i64(x: &i32) -> i64 {
   return (x * 2) as i64;
 }
 
+fn sqrt(n: usize) -> usize {
+  if n == 0 || n == 1 {
+    return n;
+  }
+
+  let mut i = 1;
+  let mut result = 1;
+
+  while result <= n {
+    i += 1;
+    result = i * i;
+  }
+
+  return i - 1;
+}
+
 // Slice an array if in range
 fn slice_arr(arr: &[i32], range: (usize, usize)) -> Result<&[i32], &'static str> {
-  println!("{}", arr.len() >= range.1);
   if arr.len() >= range.0 && arr.len() >= range.1 {
-      return Ok(&arr[(range.0 - 1)..(range.1 - 1)]);
+    return Ok(&arr[(range.0 - 1)..(range.1 - 1)]);
   }
   Err("OOB!")
 }
