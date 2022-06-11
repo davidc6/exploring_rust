f93977684fd43426d88366ddc95a35d579b02a69
David Chubabriya <david.chubabriya@gmail.com>
2022-06-11 22:13:05 +0100

    Add initial mandelbrot implementation

---
+++ cargo-projects/mandelbrot/src/main.rs
@@ -0,0 +1,76 @@
+use num::Complex;
+use std::str::FromStr;
+
+fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) { // find the location of a separator
+        None => None, // no such location is found
+        Some(index) => { // location found
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) { // get string portion before the separator and after
+                (Ok(l), Ok(r)) => Some((l, r)), // left and right values
+                _ => None
+            }
+        }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') { // parse pair of numbers first
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+fn pixel_to_point(
+    bounds: (usize, usize), 
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>
+) -> Complex<f64> {
+    let (
+        width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im
+    );
+
+    Complex { 
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64, // "as" Rust's syntax for a type conversion
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex { re: 1.25, im: -0.0625 }));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point(
+        (100, 200),
+        (25, 175),
+        Complex { re: -1.0, im: 1.0 },
+        Complex { re: 1.0, im: -1.0 }), Complex { re: -0.5, im: -0.75 })
+}
