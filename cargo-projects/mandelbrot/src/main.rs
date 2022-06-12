ee515d29878d0b5b3f843f0e7a3184b6d22b9372
David Chubabriya <david.chubabriya@gmail.com>
2022-06-12 23:15:17 +0100

    Add picture rendering functions

--- cargo-projects/mandelbrot/src/main.rs
+++ cargo-projects/mandelbrot/src/main.rs
@@ -1,7 +1,10 @@
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
 
-fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<usize> {
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
@@ -10,6 +13,45 @@ fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<usize> {
         z = z * z + c;
     }
     None
+}
+
+// opens a file and attempts to writes the image to it
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    // ? operator is a shorthand for the code below which returns error if it fails or successfully opend file
+    // let output = File::create(path)?;
+    // handle error happy and sad paths
+    let output = match File::create(filename) {
+        Ok(f) => f,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    let encoder = PNGEncoder::new(output);
+    // pixels data, width and height from bounds and bytes in pixels in 8 bit grayscale value
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
+// &mut [u8] - mutable reference to vector of unsigned integers
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixels_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = 
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                }
+        }
+    }
 }
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
@@ -31,7 +73,7 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
-fn pixel_to_point(
+fn pixels_to_point(
     bounds: (usize, usize), 
     pixel: (usize, usize),
     upper_left: Complex<f64>,
@@ -68,7 +110,7 @@ fn test_parse_complex() {
 
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(pixel_to_point(
+    assert_eq!(pixels_to_point(
         (100, 200),
         (25, 175),
         Complex { re: -1.0, im: 1.0 },
