c1d7acae37923c8fccd4c87c4c9a24383ae3869d
David Chubabriya <david.chubabriya@gmail.com>
2022-07-19 22:03:17 +0100

    Add initial cli implementation

---
+++ cargo-projects/cliapp/src/main.rs
@@ -0,0 +1,38 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("cliapp")
+        .version("0.1.0")
+        .author("David C")
+        .about("Just a test CLI app")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let t = "text";
+    let text = matches.get_many::<String>(t)
+        .unwrap()
+        .cloned()
+        .collect::<Vec<_>>();
+
+    let omit_newline = matches.contains_id("omit_newline");
+
+    let mut ending = "\n";
+
+    if omit_newline {
+        ending = "";
+    }
+
+    print!("{}{}", text.join(" "), ending);
+}
