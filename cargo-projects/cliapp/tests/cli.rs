ccbd1d54ad3f70079b1048b38c0515b5715bfe94
David Chubabriya <david.chubabriya@gmail.com>
2022-07-24 21:07:15 +0100

    Add tests

--- cargo-projects/cliapp/tests/cli.rs
+++ cargo-projects/cliapp/tests/cli.rs
@@ -16,8 +16,22 @@ fn runs_and_exist_successfully() {
 }
 
 #[test]
-fn hello1() {
+fn hello_arg_format1() {
     let mut cmd = Command::cargo_bin("cliapp").unwrap();
     let expected = String::from("Hello there\n");
     cmd.arg("Hello there").assert().success().stdout(expected);
+}
+
+#[test]
+fn hello_arg_format2() {
+    let mut cmd = Command::cargo_bin("cliapp").unwrap();
+    let expected = String::from("Hello there\n"); // same as (two spaces between args) -> echo "Hello"  "there"
+    cmd.args(vec!["Hello", "there"]).assert().success().stdout(expected);
+}
+
+#[test]
+fn hello_arg_format3() {
+    let mut cmd = Command::cargo_bin("cliapp").unwrap();
+    let expected = String::from("Hello  there");
+    cmd.args(vec!["Hello  there", "-n"]).assert().success().stdout(expected);
 }
