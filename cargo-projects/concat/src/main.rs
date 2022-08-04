87312d6022a13e5ffc73bb3cdd2d2154314e77dd
David Chubabriya <david.chubabriya@gmail.com>
2022-08-04 20:19:35 +0100

    Clean up imports

--- cargo-projects/concat/src/main.rs
+++ cargo-projects/concat/src/main.rs
@@ -1,7 +1,10 @@
-use std::io::{Write, BufReader, Read};
+use std::io::{Write, BufReader, Read, stdout};
+use std::fs::{File};
+use std::path::{Path};
 use clap::{arg, Command, ArgAction};
+use std::error::{Error};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let matching = Command::new("Concat")
         .version("0.1")
         .author("davidc6")
@@ -11,15 +14,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if matching.get_one::<String>("FILE") != None {
         let filepath_str = matching.get_one::<String>("FILE").unwrap();
-        let filepath = std::path::Path::new(filepath_str);
+        let filepath = Path::new(filepath_str);
 
         if filepath.exists() {
-            let file = std::fs::File::open(filepath)?;
+            let file = File::open(filepath)?;
             let mut buffered = BufReader::new(file);
             let mut buffer = Vec::new();
             buffered.read_to_end(&mut buffer)?;
 
-            std::io::stdout().write_all(&buffer)?;
+            stdout().write_all(&buffer)?;
         }
     }
 
