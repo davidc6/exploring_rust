5e9187d1e64c23203363d333671186c342e7e44d
David Chubabriya <david.chubabriya@gmail.com>
2022-08-31 23:14:43 +0100

    Add a simple slice type algorithm

---
+++ code-samples/slice-type/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let s = String::from("Hello world this is Rust");
+    let a = find_str(&s, 1); // find the first word
+    println!("{:?}", a);
+}
+
+fn find_str<'a>(s: &'a String, order: usize) -> &'a str {
+    let space = ' ';
+    let mut start = 0;
+    let mut finish = 0;
+    let mut count = 0;
+
+    for (index, value) in s.chars().enumerate() {
+        if value == space && start == finish {
+            start += 1;
+            continue;
+        }
+
+        if value == space {
+            count += 1;
+            if count == order {
+                return &s[start..finish]
+            }
+            start = index + 1;
+            finish = index;
+        }
+
+        finish += 1;
+    }
+
+    &s[0..1]
+}
