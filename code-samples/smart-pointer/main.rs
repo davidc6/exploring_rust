8b93d3aa98e84cb0ca92cd4098bb0091f6be7abe
David Chubabriya <david.chubabriya@gmail.com>
2022-09-10 13:36:36 +0100

    Update with comments

--- code-samples/smart-pointer/main.rs
+++ code-samples/smart-pointer/main.rs
@@ -1,6 +1,8 @@
 use std::ops::Deref;
 
 fn main() {
+    // define struct and declare a generic parameter T
+    // to hold value of any type
     struct MyPointer<T>(T);
 
     impl<T> MyPointer<T> {
@@ -9,17 +11,19 @@ fn main() {
         }
     }
 
+    // T is a generic type parameter
     impl<T> Deref for MyPointer<T> {
         type Target = T;
 
         fn deref(&self) -> &Self::Target {
+            // the value we want to access using the deref operator - *
             &self.0
         }
     }
 
-    let x = 5;
-    let y = MyPointer::new(5);
+    let x = 10;
+    let y = MyPointer::new(10);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    assert_eq!(10, x);
+    assert_eq!(10, *y);
 }
