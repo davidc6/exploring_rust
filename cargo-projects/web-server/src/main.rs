c7fbd82040380773181f3b4221aa802c5cc1696f
David Chubabriya <david.chubabriya@gmail.com>
2022-09-13 21:57:10 +0100

    Refactor

--- cargo-projects/web-server/src/main.rs
+++ cargo-projects/web-server/src/main.rs
@@ -1,4 +1,5 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write, Read}};
+use serde::{Serialize, Deserialize};
 
 enum HttpVersion {
     One,
@@ -28,6 +29,13 @@ impl Request {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+
+struct Test {
+    name: String,
+    email: String
+}
+
 struct Response {
     // http_version: 
     // status_code: 
@@ -39,79 +47,20 @@ fn process_method(line: String) -> Vec<String> {
     line.split(" ").map(String::from).collect()
 }
 
-fn process_connection(mut stream: TcpStream) {
-    let mut request = Request {
-        method: String::from(""),
-        uri: String::from(""),
-        http_version: String::from(""),
-        headers: vec![]
-    };
-    
-    // create new buffer to read the stream that wraps mutatable a mutable red to TcpStream
-    let buf = BufReader::new(&mut stream);
-    let (req): Vec<_> = buf
-        .lines() // streams of data are split here on newline byte
-        .enumerate()
-        .map(|(index, line)| {
-            let end = match line {
-                Ok(line) => {
-                    let a = match index {
-                        0 => {
-                            let res = process_method(line);
-
-                            request.method(res.get(0).unwrap().to_string());
-                            request.uri(res.get(1).unwrap().to_string());
-                            request.http_version(res.get(2).unwrap().to_string());
-                            String::from("a")
-                        },
-                        _ => {
-                            let b = match request.method {
-                                _ if request.method == String::from("GET") && !line.is_empty() => {
-                                    request.headers.push(line);
-                                    String::from("a")
-                                },
-                                _ => {
-                                    String::from("")
-                                }
-                            };
-
-                            b
-                        }
-                    };
-                    a
-                },
-                Err(e) => {
-                    let val = match e.kind() {
-                        std::io::ErrorKind::InvalidData => String::from("Check your requets for non-UTF8 data"),
-                        _ => String::from("There has been an error, check again later")
-                    };
-                    val
-                }
-            };
-            println!("{:?}", end);
-            end
-        })
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-        println!("Hello {:?}", request);
-
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    // stream.write_all(response.as_bytes()).unwrap();
-    stream.write_all(response.as_bytes()).unwrap();
-
-    // println!("{:?}", http_req);
-}
-
 fn main() {
     // bind (connect) socket to the address and port
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    println!("STARTED");
+
     // iterator over streams (open client / server connection)
     for stream in listener.incoming() {
+    println!("INSIDE");
+
         let stream = stream.unwrap();
-        process_connection(stream);
+        // println!("{:?}", stream);        
+        // process_connection(stream);
+        handle_test(stream);
         println!("Connection established");
     }
 }
