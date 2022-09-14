d00faaba628cacbdbb80bf2093da7179b6c93288
David Chubabriya <david.chubabriya@gmail.com>
2022-09-14 19:33:15 +0100

    Add process connection function

--- cargo-projects/web-server/src/main.rs
+++ cargo-projects/web-server/src/main.rs
@@ -47,6 +47,74 @@ fn process_method(line: String) -> Vec<String> {
     line.split(" ").map(String::from).collect()
 }
 
+fn process_connection(mut stream: TcpStream) {
+    let mut request = Request {
+        method: String::from(""),
+        uri: String::from(""),
+        http_version: String::from(""),
+        headers: vec![]
+    };
+    
+    // create new buffer to read the stream that wraps mutatable a mutable red to TcpStream
+    let buf = BufReader::new(&mut stream);
+
+    println!("{:?}", buf);
+
+    let (req): Vec<_> = buf
+        .lines() // streams of data are split here on newline byte
+        .enumerate()
+        .map(|(index, line)| {
+            println!("{:?}", line);
+
+            let end = match line {
+                Ok(line) => {
+                    match index {
+                        0 => {
+                            let res = process_method(line);
+
+                            request.method(res.get(0).unwrap().to_string());
+                            request.uri(res.get(1).unwrap().to_string());
+                            request.http_version(res.get(2).unwrap().to_string());
+
+                            String::from("a")
+                        },
+                        _ => {
+                            match request.method {
+                                _ if request.method == *"GET" && !line.is_empty() => {
+                                    request.headers.push(line);
+                                    String::from("a")
+                                },
+                                _ if request.method == *"POST" && !line.is_empty() => {
+                                    request.headers.push(line);
+                                    String::from("a")
+                                },
+                                _ => {
+                                    String::from("")
+                                }
+                            }
+                        }
+                    }
+                },
+                Err(e) => {
+                    match e.kind() {
+                        std::io::ErrorKind::InvalidData => String::from("Check your requets for non-UTF8 data"),
+                        _ => String::from("There has been an error, check again later")
+                    }
+                }
+            };
+
+            end
+        })
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+        println!("Hello {:?}", request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
 fn main() {
     // bind (connect) socket to the address and port
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
