c21d305c21545ff397e7eda0e1928aa80477ee44
David Chubabriya <david.chubabriya@gmail.com>
2022-09-15 22:12:17 +0100

    Add handle_test test function

--- cargo-projects/web-server/src/main.rs
+++ cargo-projects/web-server/src/main.rs
@@ -115,6 +115,104 @@ fn process_connection(mut stream: TcpStream) {
     stream.write_all(response.as_bytes()).unwrap();
 }
 
+fn handle_test(mut stream: TcpStream) -> std::io::Result<()> {
+    let  mut buf2 = BufReader::new(stream.try_clone().unwrap());
+
+    // let buf3 = std::io::Cursor::new(stream);
+    let mut buf4 = String::new();
+    // let mut buf4 = vec![];
+    // buf3.read_line(buf4);
+    let mut should_exit = false;
+    loop {
+    let mut buf4 = String::new();
+
+        let len = buf2.read_line(&mut buf4).unwrap();
+        // println!("{:?}", std::str::from_utf8(buf2.buffer()));
+        println!("{} {:?}", len, buf4);
+
+        if buf4 == "\r\n" {
+            let mut buf5 = [0; 42];
+
+            should_exit = true;
+            buf2.read_exact(&mut buf5);
+            println!("{:?}", std::str::from_utf8(&buf5).unwrap());
+            break;
+        }
+
+        // if should_exit {
+        //     break;
+        // }
+
+    }
+
+    // stream.write_all(&[1])?;
+    // stream.read_exact(&mut [0; 512])?;
+    // Ok(())
+    // println!("{:?}", buf.lines());
+
+    // let a: Vec<_> = buf.lines().map(|line| line.unwrap()).take_while(|line| !line.is_empty()).collect();
+    println!("AAA");
+    
+    // 8096
+    // let mut buf2 = [0; 173];
+    // stream.read_exact(&mut buf2)?;
+
+
+
+    println!("BBB");
+
+    let mut body_vec: Vec<u8> = vec![];
+    let mut counter = 0;
+
+    // for byte in buf2.bytes() {
+    //     if *byte.as_ref().unwrap() == 0 {
+    //         break;
+    //     }
+
+    //     if counter == 4 {
+    //         body_vec.push(byte.unwrap());
+    //         continue;
+    //     }
+
+    //     if *byte.as_ref().unwrap() == 13 {
+    //         counter += 1;
+    //         continue;
+    //     }
+
+    //     if *byte.as_ref().unwrap() == 10 {
+    //         counter += 1;
+    //         continue;
+    //     }
+
+    //     counter = 0;
+    // }
+
+
+    // let s = match std::str::from_utf8(&buf2) {
+    //     Ok(v) => v,
+    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    // };
+
+    // buf.lines()
+
+    // let zero_pos = buf2.iter().find(|&&x| x == 0);
+
+    // println!("{:?}", s);
+    // println!("{:?}", buf2);
+
+    // println!("{:?}", std::str::from_utf8(&body_vec).unwrap());
+    let s = std::str::from_utf8(&body_vec);
+    // let deserialized: Test = serde_json::from_str(std::str::from_utf8(&body_vec).unwrap()).unwrap();
+    let des: Test = serde_json::from_str(s.unwrap()).unwrap();
+
+    println!("{:?}", des);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+    Ok(())
+
+}
+
 fn main() {
     // bind (connect) socket to the address and port
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
