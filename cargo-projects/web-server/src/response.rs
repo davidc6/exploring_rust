ba441418e495c8f142c127c7bc961fddf37a0f92
David Chubabriya <david.chubabriya@gmail.com>
2022-10-15 12:31:27 +0100

    Update response

--- cargo-projects/web-server/src/response.rs
+++ cargo-projects/web-server/src/response.rs
@@ -1,71 +1,82 @@
 use std::collections::HashMap;
 
 #[derive(Default, Clone)]
-pub struct Response<'a> {
-    method: &'a str,
+pub struct Response {
+    method: String,
     status: String,
-    version: &'a str,
+    version: String,
     headers: HashMap<String, String>,
+    header: String,
     body: String
 }
 
-impl<'a> Response<'a> {
-    pub fn builder() -> ResponseBuilder<'a> {
+impl Response {
+    pub fn builder() -> ResponseBuilder {
         ResponseBuilder::new()
     }
 
-    pub fn version(&self) -> &str {
+    pub fn version(&self) -> &String {
         &self.version
     }
 
-    pub fn headers(self) -> String {
-        let map = self.headers.clone();
+    pub fn headers(&self) -> &String {
+        &self.header
+    }
+
+    pub fn body(&self) -> &String {
+        &self.body
+    }
+}
+
+#[derive(Default)]
+pub struct ResponseBuilder {
+    response: Response
+}
+
+impl ResponseBuilder {
+    fn new() -> ResponseBuilder {
+        ResponseBuilder::default()
+    }
+
+    pub fn method(mut self, method: &String) -> ResponseBuilder {
+        self.response.method = method.to_owned();
+        self
+    }
+
+    pub fn status(mut self, status: &String) -> ResponseBuilder {
+        self.response.status = status.to_owned();
+        self
+    }
+
+    pub fn header(mut self, header: (String, String)) -> ResponseBuilder {
+        self.response.headers.insert(header.0, header.1);
+        self
+    }
+
+    pub fn version(mut self, version: &String) -> ResponseBuilder {
+        self.response.version = version.to_owned();
+        self
+    }
+
+    pub fn body(mut self, body: String) -> Response {
+        // concat headers
+        let map = self.response.headers.clone();
         let mut header: String = "".into();
 
         for (k, v) in map.iter() {
             header = format!("{}{}:{}\r\n", header, k, v);
         }
 
-        header
-    }
+        self.response.header = header;
 
-    pub fn body(self) -> String {
-        self.body
-    }
-}
-
-#[derive(Default)]
-pub struct ResponseBuilder<'a> {
-    response: Response<'a>
-}
-
-impl<'a> ResponseBuilder<'a> {
-    fn new() -> ResponseBuilder<'a> {
-        ResponseBuilder::default()
-    }
-
-    pub fn method(mut self, method: &'a str) -> ResponseBuilder {
-        self.response.method = method;
-        self
-    }
-
-    pub fn status(mut self, status: &String) -> ResponseBuilder<'a> {
-        self.response.status = status.to_owned();
-        self
-    }
-
-    pub fn version(mut self, version: &'a str) -> ResponseBuilder<'a> {
-        self.response.version = version;
-        self
-    }
-
-    pub fn body(self, body: String) -> Response<'a> {
+        // instantiate Response
         Response {
             method: self.response.method,
             status: self.response.status,
             version: self.response.version,
             headers: self.response.headers,
-            body
+            body,
+            header: self.response.header
         }
     }
 }
