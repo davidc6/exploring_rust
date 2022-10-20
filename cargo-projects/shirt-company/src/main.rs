068f13728ba5a2ea039b8d39c73f1b3e23a1e176
David Chubabriya <david.chubabriya@gmail.com>
2022-10-20 21:44:47 +0100

    Add enum to Str conversion

--- cargo-projects/shirt-company/src/main.rs
+++ cargo-projects/shirt-company/src/main.rs
@@ -1,7 +1,19 @@
+use std::fmt::{Debug, Display, Formatter, Result};
+
 #[derive(Debug, Copy, Clone)]
 enum ShirtColour {
     Black,
     White
+}
+
+// implementing Display trait enables to "convert" enums to Strings
+impl Display for ShirtColour {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            ShirtColour::Black => write!(f, "Black"),
+            ShirtColour::White => write!(f, "White"),
+        }
+    }
 }
 
 struct Inventory {
@@ -12,6 +24,7 @@ impl Inventory {
     /// returns if Some variant or 
     /// calls the closure which captures an immutable reference to self Inventory 
     /// (which function cannot capture the env) if None variant
+    /// the result of
     fn giveaway(&self, user_preference: Option<ShirtColour>) -> ShirtColour {
         user_preference.unwrap_or_else(|| self.most_stocked())
     }
@@ -55,4 +68,7 @@ fn main() {
         user_pref2,
         giveaway2
     );
+
+    let colour: String = ShirtColour::Black.to_string();
+    println!("{}", colour);
 }
