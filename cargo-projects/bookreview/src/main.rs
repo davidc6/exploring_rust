5727027ea1a65883b67a690ed73a035945148e77
David Chubabriya <david.chubabriya@gmail.com>
2022-10-24 23:35:58 +0100

    Update code

--- cargo-projects/bookreview/src/main.rs
+++ cargo-projects/bookreview/src/main.rs
@@ -1,9 +1,85 @@
-use actix_web::{get, web, App, HttpServer, Responder, Result};
-use serde::Serialize;
+// use std::sync::Mutex;
+
+use std::{ops::DerefMut, sync::Mutex};
+
+use actix_web::{get, post, web, App, HttpServer, Responder, Result, HttpRequest, HttpResponse};
+use serde::{Serialize, Deserialize};
+
+// use serde_json::{Result, Value};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Book {
+    id: String,
+    title: String,
+    author: String
+}
 
 #[get("/books")]
-async fn books() -> impl Responder {
-    "Books endpoint"
+async fn get_books(data: web::Data<AppStateMutable>) -> Result<impl Responder> {
+    // let a = *req.app_data::<usize>().unwrap();
+    // println!("{:?}", a);
+    
+    let mut data = data.data.lock().unwrap();
+    data.push(Book {
+        id: "3".to_owned(),
+        title: "Title 3".to_owned(),
+        author: "Author 3".to_owned()
+    });
+    data.push(Book {
+        id: "4".to_owned(),
+        title: "Title 4".to_owned(),
+        author: "Author 4".to_owned()
+    });
+
+    // data.data.push(Book {
+    //     id: "3".to_owned(),
+    //     title: "Title 3".to_owned(),
+    //     author: "Author 3".to_owned()
+    // });
+    
+    // let app = &data.data;
+    // println!("{:?}", data);
+
+    // let books: Vec<Book> = vec![
+    //     Book {
+    //         id: "1".to_owned(),
+    //         title: "Title 1".to_owned(),
+    //         author: "Author 1".to_owned()
+    //     },
+    //     Book {
+    //         id: "2".to_owned(),
+    //         title: "Title 2".to_owned(),
+    //         author: "Author 2".to_owned()
+    //     }
+    // ];
+    // let s = serde_json::to_string(&data).unwrap();
+
+    // let b = data.downcast_ref()
+
+    // let s = format!("{:?}", data);
+    // let b: String = serde_json::from_str(&s).unwrap();
+    // Ok(web::Json(s))
+    // let w = data
+
+        // println!("{:?}", data);
+
+    // let b = serde_json::
+
+    Ok(HttpResponse::Ok().json(&**data))
+
+    // Ok(web::Json(data))
+}
+
+#[post("/books")]
+async fn post_books(req: HttpRequest, counter: web::Data<AppState>) -> impl Responder {
+    // TODO: unique id for a book
+    // let a = *req.app_data::<AppStateWithCounter>().unwrap();
+    // println!("{:?}", a);
+    // counter.counter.set(counter.counter.get());
+
+
+
+    "POST Books endpoint"
 }
 
 #[derive(Serialize)]
@@ -19,12 +95,44 @@ async fn ping() -> Result<impl Responder> {
     Ok(web::Json(health))
 }
 
+// #[derive(Debug, Clone, Copy)]
+struct AppState {
+    str: String
+}
+
+#[derive(Debug, Serialize)]
+struct AppStateNew {
+    data: Vec<Book>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AppStateMutable {
+    data: Mutex<Vec<Book>>
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let data = web::Data::new(AppStateMutable {
+        data: Mutex::new(vec![
+            Book {
+                id: "1".to_owned(),
+                title: "Title 1".to_owned(),
+                author: "Author 1".to_owned()
+            },
+            Book {
+                id: "2".to_owned(),
+                title: "Title 2".to_owned(),
+                author: "Author 2".to_owned()
+            }
+        ]),
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(data.clone())
             .service(ping)
-            .service(books)
+            .service(get_books)
+            .service(post_books)
     })
     .bind(("127.0.0.1", 7878))?
     .run()
