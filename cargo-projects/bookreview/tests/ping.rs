a46bc21251edd2664f09365e98abc1067aa39dba
David Chubabriya <david.chubabriya@gmail.com>
2022-11-14 23:37:44 +0000

    Test invalid input

--- cargo-projects/bookreview/tests/ping.rs
+++ cargo-projects/bookreview/tests/ping.rs
@@ -34,6 +34,35 @@ async fn follow_returns_200_when_valid_data() {
     assert_eq!(200, res.status().as_u16());
 }
 
+#[tokio::test]
+async fn follow_returns_400_when_missing_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+    // table-driven / parametised test
+    let cases = vec![
+        ("name=john%doe", "missing email"),
+        ("email=john.doe%40gmail.com", "missing name"),
+        ("", "missing name and email")
+    ];
+
+    for (body, err_msg) in cases {
+        let res = client
+            .post(format!("{}/follows", addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            res.status(),
+            "The endpoint should have failed with 400 when the payload was {}",
+            err_msg
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
