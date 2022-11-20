8e19abe908d6c58180e26515783b6d40a255e57a
David Chubabriya <david.chubabriya@gmail.com>
2022-11-20 23:15:01 +0000

    Update migrations

--- cargo-projects/bookreview/migrations/20221122213954_create_follows_table.sql
+++ cargo-projects/bookreview/migrations/20221122213954_create_follows_table.sql
@@ -1,1 +1,9 @@
--- Add migration script here
+-- migrations/{timestamps}_create_follows_table.sql
+-- Create Follows Table
+CREATE TABLE follows(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    followed_at timestamptz NOT NULL
+);
