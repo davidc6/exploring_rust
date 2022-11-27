15e6a53642977f1688f8a54f565ccb949a76eedd
David Chubabriya <david.chubabriya@gmail.com>
2022-11-27 08:39:30 +0000

    Update tests

--- cargo-projects/bookreview/tests/ping.rs
+++ cargo-projects/bookreview/tests/ping.rs
@@ -1,15 +1,15 @@
 //! tests/health_check.rs
 use std::{net::TcpListener};
 use bookreview::configuration::{configuration};
-use sqlx::{PgConnection, Connection};
+use sqlx::{PgConnection, Connection, PgPool};
 
 #[tokio::test]
 async fn ping_works() {
-    let address = spawn_app();
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/ping", address))
+        .get(format!("{}/ping", test_app.addr))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -20,11 +20,11 @@ async fn ping_works() {
 
 #[tokio::test]
 async fn follow_returns_200_when_valid_data() {
-    let addr = spawn_app();
+    let test_app = spawn_app().await;
     let conf = configuration().expect("Failed to get the config");
     let conn_str = conf.database.conn_str();
 
-    let conn = PgConnection::connect(&conn_str)
+    let mut conn = PgConnection::connect(&conn_str)
         .await
         .expect("Failed to connect to Postgres database");
 
@@ -32,7 +32,7 @@ async fn follow_returns_200_when_valid_data() {
 
     let body = "name=john%20doe&email=john.doe%40gmail.com";
     let res = client
-        .post(format!("{}/follows", addr))
+        .post(format!("{}/follows", test_app.addr))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -40,11 +40,19 @@ async fn follow_returns_200_when_valid_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, res.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM follows")
+        .fetch_one(&mut conn)
+        .await
+        .expect("Failed to fetch saved follows.");
+
+    assert_eq!(saved.email, "a@a.com");
+    assert_eq!(saved.name, "a");
 }
 
 #[tokio::test]
 async fn follow_returns_400_when_missing_data() {
-    let addr = spawn_app();
+    let addr = spawn_app().await;
     let client = reqwest::Client::new();
     // table-driven / parametised test
     let cases = vec![
@@ -55,7 +63,7 @@ async fn follow_returns_400_when_missing_data() {
 
     for (body, err_msg) in cases {
         let res = client
-            .post(format!("{}/follows", addr))
+            .post(format!("{}/follows", addr.addr))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
             .send()
@@ -71,18 +79,38 @@ async fn follow_returns_400_when_missing_data() {
     }
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+#[derive(Debug)]
+pub struct TestApp {
+    pub addr: String,
+    pub db_pool: PgPool
+} 
+
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
 
     // OS assigned port address
     let port = listener.local_addr().unwrap().port();
 
-    let server = bookreview::startup::run(listener).expect("Failed to bind address");
+    // Return application address to the caller
+    let addr = format!("http://127.0.0.1:{}", port);
+
+    let conf = configuration().expect("Failed to read the config");
+    let conn_pool = PgPool::connect(&conf.database.conn_str())
+        .await
+        .expect("Failed to connect to PG");
+
+
+    let server = bookreview::startup::run(listener, conn_pool.clone())
+        .expect("Failed to bind address");
 
     // Server gets launched as a background task
     // let is non-binding hence no use for it here
     let _ = tokio::spawn(server);
 
-    // Return application address to the caller
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        addr,
+        db_pool: conn_pool
+    }
+
 }
