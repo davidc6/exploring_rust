536895f94092c6522955ce0b1b858f0e8253d0f0
David Chubabriya <david.chubabriya@gmail.com>
2023-01-16 23:15:11 +0000

    Building up the parser

--- cargo-projects/vivs/src/server.rs
+++ cargo-projects/vivs/src/server.rs
@@ -5,28 +5,98 @@ enum DataTypes {
     Array
 }
 
-fn parse_array(stream: &[u8]) -> String {
-    let mut arr_size = Vec::new();
-
-    for (index, char) in stream.iter().enumerate() {
-        println!("{} {}", index, *char as char);
-
-        if index == 0 {
-            // arr_size.push(*char as char);
-            let mut it = stream.iter();
-            let mut next = it.next();
-
-            while next != Some(&13) {
-                println!("HERE {}", *next.unwrap() as char);
-                arr_size.push(*next.unwrap() as char);
-                next = it.next();
-            }
-            continue;
+// carriage return 
+fn parse_until_crlf(stream: &[u8]) -> Result<(&[u8], &[u8]), String> {
+    // println!("{:?}", stream);
+    for (index, val) in stream.iter().enumerate() {
+        if val == &b'\r' && stream[index + 1] == b'\n' {
+            // result and rest
+            return Ok((&stream[..index], &stream[index + 2..]));
         }
     }
 
-    println!("Size {:?}", arr_size.iter().collect::<String>());
-    return arr_size.iter().collect::<String>();
+    // error
+    return Err("Err".to_owned());
+}
+
+fn parse_bulk_strings(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
+    // 4\r\nPING\r\n
+
+    let array = parse_until_crlf(buffer);
+
+    let str_size = array.as_ref().unwrap().0;
+    let left = array.as_ref().unwrap().1;
+
+    // for (index, val) in buffer.iter().enumerate() {
+    //     if val == &b'\r' && buffer[index + 1] == b'\n' {
+    //         // result and rest
+    //         return Ok((&buffer[..index], &buffer[index + 2..]));
+    //     }
+    // }
+
+    println!("{:?} {:?}", str_size, left);
+
+    Ok((Vec::new(), &[2]))
+}
+
+fn parse_array(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
+    let array = parse_until_crlf(buffer);
+
+    let arr_size = array.as_ref().unwrap().0;
+    let left = array.as_ref().unwrap().1;
+    
+    println!("ARR {:?}", arr_size);
+
+    let mut vec: Vec<_> = Vec::with_capacity(std::str::from_utf8(arr_size).unwrap().parse::<u64>().unwrap() as usize);
+    // vec.push(b"ds");
+    for val in left.iter().enumerate() {
+        let res = parse(left);
+    }
+
+    Ok((vec, left))
+
+    // let mut arr_size = Vec::new();
+
+    // for (index, char) in stream.iter().enumerate() {
+    //     println!("{} {}", index, *char as char);
+
+    //     if index == 0 {
+    //         // arr_size.push(*char as char);
+    //         let mut it = stream.iter();
+    //         let mut next = it.next();
+
+    //         while next != Some(&13) {
+    //             println!("HERE {}", *next.unwrap() as char);
+    //             arr_size.push(*next.unwrap() as char);
+    //             next = it.next();
+    //         }
+    //         continue;
+    //     }
+    // }
+
+    // println!("Size {:?}", arr_size.iter().collect::<String>());
+    // return arr_size.iter().collect::<String>();
+}
+
+fn parse(buffer: &[u8]) {
+    print!("Buffer {:?}", buffer);
+
+    if let Some(data_type) = buffer.first() {
+        let data_type = match data_type {
+            b'*' => parse_array(&buffer[1..buffer.len()]),
+            b'$' => parse_bulk_strings(&buffer[1..buffer.len()]),
+                // let arr_let = &buffer[0..2];
+                // println!("{:?}", arr_let);
+            _ => todo!()
+        };
+
+        println!("{:?}", data_type);
+
+        
+
+    } else {
+        panic!("Unexpected");
+    }
 }
 
 async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) {
@@ -35,26 +105,29 @@ async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) {
 
     let mut buffer: Vec<u8> = vec![];
     reader.read_to_end(&mut buffer).await.expect("Error");
+    let slice = &buffer[0..buffer.len()];
 
-    if let Some(data_type) = buffer.first() {
-        let data_type = match *data_type as char {
-            '*' => {
+    parse(slice);
 
-                parse_array(&buffer[1..]);
+    // if let Some(data_type) = buffer.first() {
+    //     let data_type = match *data_type as char {
+    //         '*' => {
 
-                // let arr_let = &buffer[0..2];
-                // println!("{:?}", arr_let);
-            },
-            _ => todo!()
-        };
+    //             parse_array(&buffer[1..]);
 
-        // println!("{:?}", data_type);
+    //             // let arr_let = &buffer[0..2];
+    //             // println!("{:?}", arr_let);
+    //         },
+    //         _ => todo!()
+    //     };
+
+    //     // println!("{:?}", data_type);
 
         
 
-    } else {
-        panic!("Unexpected");
-    }
+    // } else {
+    //     panic!("Unexpected");
+    // }
 
 
 
