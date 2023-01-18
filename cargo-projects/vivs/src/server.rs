3419612e38c6d12bdf6c8641b53a46c0fdee047b
David Chubabriya <david.chubabriya@gmail.com>
2023-01-18 21:32:01 +0000

    Improve byte stream reads

--- cargo-projects/vivs/src/server.rs
+++ cargo-projects/vivs/src/server.rs
@@ -5,13 +5,14 @@ enum DataTypes {
     Array
 }
 
+
+
 // carriage return 
 fn parse_until_crlf(stream: &[u8]) -> Result<(&[u8], &[u8]), String> {
-    // println!("{:?}", stream);
     for (index, val) in stream.iter().enumerate() {
         if val == &b'\r' && stream[index + 1] == b'\n' {
             // result and rest
-            return Ok((&stream[..index], &stream[index + 2..]));
+            return Ok((&stream[..index], &stream[index + 2..stream.len()]));
         }
     }
 
@@ -27,29 +28,10 @@ fn parse_bulk_strings(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
     let str_size = array.as_ref().unwrap().0;
     let left = array.as_ref().unwrap().1;
 
-    // for (index, val) in buffer.iter().enumerate() {
-    //     if val == &b'\r' && buffer[index + 1] == b'\n' {
-    //         // result and rest
-    //         return Ok((&buffer[..index], &buffer[index + 2..]));
-    //     }
-    // }
-
-    println!("STR {:?} {:?}", str_size, left);
-
     let mut vec: Vec<_> = Vec::with_capacity(std::str::from_utf8(str_size).unwrap().parse::<u64>().unwrap() as usize);
 
-    for val in left.iter().enumerate() {
-        let pos = str_size[0] as char;
-        println!("START {}", pos.to_digit(10).unwrap());
-        let res = parse(&left[pos.to_digit(10).unwrap() as usize..]);
-    }
-
-
-    // let mut v = Vec::new();
-    
-    // for item in left {
-    //     v.push(item);
-    // }
+    let pos = str_size[0] as char;
+    let res = parse(&left[pos.to_digit(10).unwrap() as usize..]);
 
     Ok((vec, left))
 }
@@ -60,70 +42,56 @@ fn parse_array(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
     let arr_size = array.as_ref().unwrap().0;
     let left = array.as_ref().unwrap().1;
     
-    // println!("ARR {:?}", arr_size);
-
     let mut vec: Vec<_> = Vec::with_capacity(std::str::from_utf8(arr_size).unwrap().parse::<u64>().unwrap() as usize);
 
-    // vec.push(b"ds");
-    for val in left.iter().enumerate() {
-        let res = parse(left);
-    }
+    let res = parse(left);
 
     Ok((vec, left))
-
-    // let mut arr_size = Vec::new();
-
-    // for (index, char) in stream.iter().enumerate() {
-    //     println!("{} {}", index, *char as char);
-
-    //     if index == 0 {
-    //         // arr_size.push(*char as char);
-    //         let mut it = stream.iter();
-    //         let mut next = it.next();
-
-    //         while next != Some(&13) {
-    //             println!("HERE {}", *next.unwrap() as char);
-    //             arr_size.push(*next.unwrap() as char);
-    //             next = it.next();
-    //         }
-    //         continue;
-    //     }
-    // }
-
-    // println!("Size {:?}", arr_size.iter().collect::<String>());
-    // return arr_size.iter().collect::<String>();
 }
 
-fn parse(buffer: &[u8]) {
+fn parse(buffer: &[u8]) -> Result<(), ()> {
     print!("Buffer {:?}\n", buffer);
 
     if let Some(data_type) = buffer.first() {
         let data_type = match data_type {
             b'*' => parse_array(&buffer[1..buffer.len()]),
             b'$' => parse_bulk_strings(&buffer[1..buffer.len()]),
-                // let arr_let = &buffer[0..2];
-                // println!("{:?}", arr_let);
+            b'\r' => Ok((Vec::with_capacity(1), buffer)),
             _ => Ok((Vec::with_capacity(1), buffer))
         };
 
-        // println!("{:?}", data_type);
-
-        
-
+        return Ok(());
     } else {
-        panic!("Unexpected");
+        // run the command
+        // panic!("Unexpected");
+        return Ok(());
     }
+
+    return Ok(());
 }
 
-async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) {
+async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) -> std::io::Result<()> {
     let (read, write) = stream.split();
     let mut reader = BufReader::new(read);
 
-    let mut buffer: Vec<u8> = vec![];
-    reader.read_to_end(&mut buffer).await.expect("Error");
+    // let mut buffer: Vec<u8> = vec![];
+    // reader.read_to_end(&mut buffer).await?;
+    let mut buffer = [0; 14];
+    reader.read_exact(&mut buffer).await?;
+
     let slice = &buffer[0..buffer.len()];
 
     parse(slice);
+
+    match write.try_write(b"+PONG\r\n") {
+        Ok(val) => {
+            println!("Final read: {:?}", val);
+        }
+        Err(e) => {
+
+        }
+    };
+    // return;
 
     // if let Some(data_type) = buffer.first() {
     //     let data_type = match *data_type as char {
@@ -192,6 +160,7 @@ async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) {
 
     //     buffer.clear();
     // }
+    Ok(())
 }
 
 pub async fn start(addr: String, port: String) -> std::io::Result<()> {
