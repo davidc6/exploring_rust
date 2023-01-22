c3a99ec1141a7a6dfa82669ecbc5865c775f1b9e
David Chubabriya <david.chubabriya@gmail.com>
2023-01-22 21:07:34 +0000

    Clean up the parser

--- cargo-projects/vivs/src/server.rs
+++ cargo-projects/vivs/src/server.rs
@@ -19,35 +19,7 @@ fn parse_until_crlf(stream: &[u8]) -> Result<(&[u8], &[u8]), String> {
     return Err("Err".to_owned());
 }
 
-// fn parse_bulk_strings(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
-    // 4\r\nPING\r\n
-
-//     let array = parse_until_crlf(buffer);
-
-//     let str_size = array.as_ref().unwrap().0;
-//     let left = array.as_ref().unwrap().1;
-
-//     let mut vec: Vec<_> = Vec::with_capacity(std::str::from_utf8(str_size).unwrap().parse::<u64>().unwrap() as usize);
-
-//     let pos = str_size[0] as char;
-//     let res = parse(&left[pos.to_digit(10).unwrap() as usize..]);
-
-//     Ok((vec, left))
-// }
-
-// fn parse_array(buffer: &[u8]) -> Result<(Vec<&[u8]>, &[u8]), ()> {
-//     let array = parse_until_crlf(buffer);
-
-//     let arr_size = array.as_ref().unwrap().0;
-//     let left = array.as_ref().unwrap().1;
-    
-//     let mut vec: Vec<_> = Vec::with_capacity(std::str::from_utf8(arr_size).unwrap().parse::<u64>().unwrap() as usize);
-
-//     let res = parse(left);
-
-//     Ok((vec, left))
-// }
-
+// 4\r\nPING\r\n
 
 struct BufSplit(usize, usize);
 
@@ -77,16 +49,11 @@ type Buffer = [u8];
 fn parse_word(buffer: &Buffer, byte_position: usize) -> Option<(usize, BufSplit)> {
     // edge cases?
 
-    // tracks end of the word
-    let mut end_position = byte_position;
-
-    for val in &buffer[byte_position..] {
-        end_position += 1;
-
+    for (index, val) in buffer[byte_position..].iter().enumerate() {
         if val == &b'\r' {
             // byte_position + end + 2 is start + end of word + \r\n -> next position
             // byte_position - start, byte_position + end - end of actual word
-            return Some((byte_position + end_position + 2, BufSplit(byte_position, byte_position + end_position)));
+            return Some((byte_position + index + 2, BufSplit(byte_position, byte_position + index)));
         }
     }
 
@@ -100,7 +67,10 @@ fn parse_integer(buffer: &Buffer, byte_position: usize) -> Result<Option<(usize,
     match parse_word(buffer, byte_position) {
         Some((position, parsed_word)) => {
             let string = str::from_utf8(parsed_word.as_slice(buffer)).map_err(|_| ParseError::Int)?;
+
+            // error does not propogate
             let integer = string.parse::<i64>().map_err(|_| ParseError::Int)?;
+
             Ok(Some((position, integer)))
         }
         None => Ok(None),
@@ -111,10 +81,13 @@ fn parse_integer(buffer: &Buffer, byte_position: usize) -> Result<Option<(usize,
 fn parse_array(buffer: &Buffer, byte_position: usize) -> ParseResult {
     match parse_integer(buffer, byte_position)? {
         None => Ok(None),
+
         Some((position, number_of_arr_elements)) => {
-            // hard-coded
-            let mut v = Vec::with_capacity(1);
-            v.push(BufSplit(1, 2));
+            println!("{} {} {}", byte_position, position, number_of_arr_elements);
+            
+            // TODO - very hard-coded value 
+            let mut v = Vec::with_capacity(number_of_arr_elements as usize);
+            v.push(BufSplit(byte_position, position));
             Ok(Some((3, PartBuf::Array(v))))
         }
     }
@@ -122,7 +95,22 @@ fn parse_array(buffer: &Buffer, byte_position: usize) -> ParseResult {
     // return Ok(None);
 }
 
+fn parse_bulk_str(buffer: &Buffer, byte_position: usize) -> ParseResult {
+    // number of chars in the string of each element in the array
+    match parse_integer(buffer, byte_position)? {
+        None => Ok(None),
+        Some((position, length)) => {
+            let total = position + length as usize;
+            let s = PartBuf::String(BufSplit(position, total));
+            Ok(Some((total + 2, s)))
+        }
+    }
+}
+
+/// Example stream to parse: *1\r\n$4\r\nPING\r\n
 fn parse(buffer: &Buffer, byte_position: usize) -> ParseResult {
+    println!("{:?}", buffer);
+
     // we need to check the buffer since the buffer could potentially be empty
     if buffer.is_empty() {
         return Ok(None);
@@ -130,40 +118,28 @@ fn parse(buffer: &Buffer, byte_position: usize) -> ParseResult {
 
     // call relevant parser and process a chunk by matching on a data type
     // byte_position + 1 == skips the data type symbol when parsing
-    match buffer[byte_position] {
+    let i = match buffer[byte_position] {
         b'*' => parse_array(buffer, byte_position + 1),
+        b'$' => parse_bulk_str(buffer, byte_position + 1),
         _ => Err(ParseError::Unknown)
-    }
+    };
+
+    return i;
 }
-
-// fn parse(buffer: &[u8]) -> Result<(), ()> {
-//     println!("Buffer {:?}\n", buffer);
-
-//     if let Some(data_type) = buffer.first() {
-//         let data_type = match data_type {
-//             b'*' => parse_array(&buffer[1..buffer.len()]),
-//             b'$' => parse_bulk_strings(&buffer[1..buffer.len()]),
-//             b'\r' => Ok((Vec::with_capacity(1), buffer)),
-//             _ => Ok((Vec::with_capacity(1), buffer))
-//         };
-
-//         return Ok(());
-//     } else {
-//         // run the command
-//         // panic!("Unexpected");
-//         return Ok(());
-//     }
-
-//     return Ok(());
-// }
 
 async fn handle_stream(mut stream: TcpStream, addr: std::net::SocketAddr) -> std::io::Result<()> {
     let (read, write) = stream.split();
-    let reader = BufReader::new(read);
-    let buffer = reader.buffer();
-    let commands = parse(buffer, 0);
+    let mut reader = BufReader::new(read);
 
-    // result?
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer).await?;
+
+    // let buffer = reader.read_to_end(read);
+    let commands = parse(&buffer, 0);
+
+    // TODO - respond
+    // 4\r\nPING
+    write.try_write(b"+PONG\n");
 
     Ok(())
 }
