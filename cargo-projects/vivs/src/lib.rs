8b69e19e4ad3d29dd6c683f902738855546345dd
David Chubabriya <david.chubabriya@gmail.com>
2023-02-26 11:36:59 +0000

    Add lib.rs

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -1,9 +1,20 @@
-
 pub mod db;
 pub use db::DataStore;
+pub use db::DataStoreWrapper;
+
+pub mod server;
 
 pub mod listener;
 pub use listener::Listener;
 
 pub mod handler;
 pub use handler::Handler;
+
+pub mod connection;
+pub use connection::Connection;
+
+pub mod commands;
+pub use commands::Command;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Error>;
