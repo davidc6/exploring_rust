7f2aaa0d91f57c006b229d316508706522e6c689
David Chubabriya <david.chubabriya@gmail.com>
2023-03-06 20:11:37 +0000

    Clean up main.rs

--- cargo-projects/vivs/src/main.rs
+++ cargo-projects/vivs/src/main.rs
@@ -5,11 +5,7 @@ pub async fn main() -> vivs::Result<()> {
     let addr = "127.0.0.1".to_string();
     let port = "6379".to_string();
 
-    // Original
-    // server::start(addr, port).await?;
-
-    // Improvement - splitting functionality into mods
-    server::run(addr, port).await?;
+    server::start(addr, port).await?;
 
     Ok(())
 }
