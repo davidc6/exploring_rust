ad79bab9e2907ef3c6c9fe958688bcf35c156afb
David Chubabriya <david.chubabriya@gmail.com>
2023-03-14 21:17:01 +0000

    Update main file

--- cargo-projects/vivs/src/main.rs
+++ cargo-projects/vivs/src/main.rs
@@ -2,7 +2,7 @@ use vivs::server;
 
 #[tokio::main]
 pub async fn main() -> vivs::Result<()> {
-    let addr = "127.0.0.1".to_string();
+    let addr = "127.0.0.1".to_string(); // TODO - extract into a config file
     let port = "6379".to_string();
 
     server::start(addr, port).await?;
