26bb0821c04a144e3a17ebb2e21ee9923b65c4f2
David Chubabriya <david.chubabriya@gmail.com>
2023-03-16 21:15:03 +0000

    Update db wrapper

--- cargo-projects/vivs/src/db.rs
+++ cargo-projects/vivs/src/db.rs
@@ -19,6 +19,8 @@ pub struct DataStoreWrapper {
     pub db: Arc<RwLock<DataStore>>,
 }
 
+// Enables wrapping of a hashmap in a RwLock which is a lock that enables multiple readers and one writer at a time,
+// which in turn gets wrapped in an Arc - a thread-safe reference counting pointer
 impl DataStoreWrapper {
     pub fn new() -> Self {
         Self {
