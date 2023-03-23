5d6266366b4720ddd7f513e4a6b03dc1ea5aaa6f
David Chubabriya <david.chubabriya@gmail.com>
2023-03-23 22:03:11 +0100

    Update set command example

--- cargo-projects/vivs/scripts/set.sh
+++ cargo-projects/vivs/scripts/set.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# {#1} - number of characters in in the first command line argument (e.g. hello is 5)
+# example *3\r\n$3\r\nSET\r\n$4\r\nkey\r\n$5\r\nvalue\r\n
+# 
+# *3
+# An array of 3 items (commands)
+#
+# $3
+# SET
+#
+# $3
+# key
+#
+# $5
+# value
+# 
+# {#1} - number of characters in in the first command line argument (e.g. key is 3)
 # $1 - first command line argument (e.g. hello)
-# {#2} - number of characters in in the second command line argument (e.g. hi is 2)
+# {#2} - number of characters in in the second command line argument (e.g. value is 5)
 # $2 - second command line argument (e.g. hi)
 printf "*3\r\n\x243\r\nSET\r\n\x24${#1}\r\n$1\r\n\x24${#2}\r\n$2\r\n" | nc -C -N 127.0.0.1 6379
