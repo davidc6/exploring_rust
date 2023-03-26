ec1b7dc43fc7aa72c6a0cadfb9eb1c1e1b9e3c77
David Chubabriya <david.chubabriya@gmail.com>
2023-03-26 18:35:49 +0100

    Add comment to command module

--- cargo-projects/vivs/src/commands/mod.rs
+++ cargo-projects/vivs/src/commands/mod.rs
@@ -1,6 +1,7 @@
 use crate::{Connection, Result};
 pub use ping::Ping;
 use std::io::Error;
+use std::result::Result as NativeResult;
 
 mod ping;
 
@@ -9,9 +10,14 @@ pub enum Command {
 }
 
 impl Command {
-    pub fn parse_cmd() -> std::result::Result<Command, Error> {
+    // this method should parse a network frame/data unit instead of returning the hard-coded command
+    // the frame essential is a redis command and for now this library will only support an array type
+    // ref: https://redis.io/docs/reference/protocol-spec/#resp-arrays
+    pub fn parse_cmd() -> NativeResult<Command, Error> {
         Ok(Command::Ping(Ping::new()))
     }
+
+    // pub fn parse_command_from_frame() -> NativeResult<Command, Error> {}
 
     pub async fn run(self, conn: Connection) -> Result<()> {
         match self {
