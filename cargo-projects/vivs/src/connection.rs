b8b12c40c5e73cc080565fc75c4ee4d40ab1451c
David Chubabriya <david.chubabriya@gmail.com>
2023-03-27 21:17:24 +0100

    Start implementing parser

--- cargo-projects/vivs/src/connection.rs
+++ cargo-projects/vivs/src/connection.rs
@@ -1,5 +1,5 @@
-use bytes::{Bytes, BytesMut};
-use std::io::{self};
+use bytes::{buf, Buf, Bytes, BytesMut};
+use std::io::{self, Cursor};
 use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 pub struct Connection {
@@ -25,13 +25,30 @@ impl Connection {
             // writer: BufWriter::new(write)
             // writer: BufWriter::new(write),
             stream,
-            buffer: BytesMut::with_capacity(1024 * 4),
+            buffer: BytesMut::with_capacity(1024), // 1kb, for now but mostly will need to increase in the future
         }
     }
 
     // TODO: frame reading will happen here
-    pub async fn read_chunk() {
-        todo!()
+    pub async fn read_chunk(&self) {
+        todo!();
+
+        // Enables to track location in the buffer by using Cursor which provides seek functionality
+        // by wrapping an underlying buffer (in our case BytesMut)
+        let mut buff = Cursor::new(&self.buffer[..]);
+
+        match buff.get_u8() {
+            b'*' => {
+                let current_position = buff.position() as usize; // will be first position
+                let end_position = buff.get_ref().len() - 1; // second to last byte
+
+                for i in current_position..end_position {
+                    if buff.get_ref()[i] == b'\r' && buff.get_ref()[i + 1] == b'\n' {
+                        buff.set_position((i + 2) as u64);
+                    }
+                }
+            }
+        }
     }
 
     // Write chunk of data / frame to the stream
