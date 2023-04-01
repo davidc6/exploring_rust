6fb2c5de3c66dd7c5f0504e52f13e4b77b05d400
David Chubabriya <david.chubabriya@gmail.com>
2023-04-01 06:14:27 +0100

    Update connection.rs to read command data type

--- cargo-projects/vivs/src/connection.rs
+++ cargo-projects/vivs/src/connection.rs
@@ -1,11 +1,15 @@
 use bytes::{buf, Buf, Bytes, BytesMut};
 use std::io::{self, Cursor};
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+use crate::Result;
 
 pub struct Connection {
     // writer: BufWriter<TcpStream>
     // writer: BufWriter<WriteHalf<'a>>,
-
     // reader: BufReader<ReadHalf>,
     // reader: BufReader<ReadHalf<'a>>,
     stream: TcpStream,
@@ -22,7 +26,7 @@ impl Connection {
         Connection {
             // writer: BufWriter::new(write),
             // reader: BufReader::new(read),
-            // writer: BufWriter::new(write)
+            // writer: BufWriter::new(write),
             // writer: BufWriter::new(write),
             stream,
             // BytesMut is a unique reference into a continuguos slice of memory
@@ -32,29 +36,70 @@ impl Connection {
     }
 
     // TODO: frame reading will happen here
-    pub async fn read_chunk(&self) {
-        // Enables to track location in the buffer by using Cursor which provides seek functionality
-        // by wrapping an underlying buffer (in our case BytesMut)
-        // self.buffer is referring to the slice (full range) of the buffer (BytesMut)
-        let mut buff = Cursor::new(&self.buffer[..]);
+    pub async fn read_chunk(&mut self) -> Result<u64> {
+        // Pull bytes from the source (self.stream - TcpStream) into the provided buffer
+        self.stream.read_buf(&mut self.buffer).await?;
+        // Cursor enables to track location in the buffer by providing seek functionality
+        // It wraps the underlying buffer (in our case BytesMut)
+        // In this case self.buffer refers to the slice (full range) of the buffer (BytesMut)
+        let mut cursored_buffer = Cursor::new(&self.buffer[..]);
 
-        match buff.get_u8() {
+        // cursored_buffer.get_u8();
+        // println!("{:?}", &self.buffer[..]);
+
+        // we need to scan the cursored_buffer and find end of line
+        // get end of buffer / hard-coded for now
+        // last index in the array
+        // let end = (cursored_buffer.get_ref().len() - 1) as u64;
+
+        // // end of line
+        // cursored_buffer.set_position(end + 2);
+
+        // println!("{:?}", cursored_buffer.remaining());
+
+        // if !cursored_buffer.has_remaining() {
+        //     return Err("error".into());
+        // }
+
+        // since buffer is [u8], we use get_u8() to get the first byte from it
+        // this also advances the position by one
+        // the first byte determines the data type
+        // e.g. * is Array
+        match cursored_buffer.get_u8() {
             b'*' => {
-                let current_position = buff.position() as usize; // will be first position
-                let end_position = buff.get_ref().len() - 1; // second to last byte
+                //     let current_position = cursored_buffer.position() as usize; // will be first position
+                //     let end_position = &cursored_buffer.get_ref().len() - 1; // second to last byte
 
-                for position in current_position..end_position {
-                    // we get the reference to the underlying value in the cursor
-                    // if we find that at some point there's \r followed by \n then we have a line
-                    // we then update position in the buffer to the start of the next line
-                    // *1\r\n\x244\r\nPING\r\n\r\n - [*1\r\n] line 1, [x244\r\n] line 2, [PING\r\n\r\n] line 3
-                    if buff.get_ref()[position] == b'\r' && buff.get_ref()[position + 1] == b'\n' {
-                        buff.set_position((position + 2) as u64);
-                    }
-                }
+                //     let mut command_length: u64 = 0;
+
+                //     // iterate over the buffer and identify a end of a line
+                //     // then return the line
+                //     for position in current_position..end_position {
+                //         // we get the reference to the underlying value in the cursor
+                //         // if we find that at some point there's \r followed by \n then we have a line
+                //         // we then update position in the buffer to the start of the next line
+                //         // *1\r\n\x244\r\nPING\r\n\r\n - [*1\r\n] line 1, [x244\r\n] line 2, [PING\r\n\r\n] line 3
+                //         if cursored_buffer.get_ref()[position] == b'\r' && cursored_buffer.get_ref()[position + 1] == b'\n' {
+                //             cursored_buffer.set_position((position + 2) as u64);
+                //             // get usize by first converting slice in array
+                //             // then converting from native endian int value to usize
+                //             command_length = u64::from_be_bytes(
+                //                 cursored_buffer.get_ref()[current_position..position]
+                //                     .try_into()
+                //                     .unwrap(),
+                //             );
+                //             // return Ok(command_length);
+                //             break;
+                //             // cursored_buffer.get_ref()[current_position..position];
+                //         }
+                //     }
+
+                //     Ok(command_length)
             }
             _ => unimplemented!(),
         }
+
+        Ok(1)
     }
 
     // Write chunk of data / frame to the stream
