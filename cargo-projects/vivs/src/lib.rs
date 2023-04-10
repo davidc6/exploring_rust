1cfb154b0e85771d1f4cb43d8a21f626fd31a363
David Chubabriya <david.chubabriya@gmail.com>
2023-04-10 20:11:18 +0100

    Export data_chunk.rs

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -16,5 +16,7 @@ pub use connection::Connection;
 pub mod commands;
 pub use commands::Command;
 
+pub mod data_chunk;
+
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
