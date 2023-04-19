379cff7390fa2f1e933e6bba7f517b8bbfd604de
David Chubabriya <david.chubabriya@gmail.com>
2023-04-19 21:14:46 +0100

    Reading and processing streams is now in handler.rs

--- cargo-projects/vivs/src/handler.rs
+++ cargo-projects/vivs/src/handler.rs
@@ -6,10 +6,11 @@ pub struct Handler {
 }
 
 impl Handler {
-    pub async fn run(self) -> Result<()> {
-        println!("Hello");
-
+    pub async fn run(mut self) -> Result<()> {
         // TODO: read a frame, should probably live in connection
+        // read bits that host/client can send (frame)
+        // this should return array of commands which will later parse
+        let number = self.connection.read_and_process_stream().await?;
 
         // TODO get the command interator
         let command = Command::parse_cmd().unwrap();
