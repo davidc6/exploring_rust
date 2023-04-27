0eab237fb1beb9116ef3de1830d6c5742beba42b
David Chubabriya <david.chubabriya@gmail.com>
2023-04-27 23:14:19 +0100

    Comment ping script command

--- cargo-projects/vivs/scripts/ping.sh
+++ cargo-projects/vivs/scripts/ping.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# - *1 - data type, number of elements
+# - $4 - data type, number of chars in string
+# - PING - string which is the actual command
+
 printf '*1\r\n\x244\r\nPING\r\n' | nc -C -N 127.0.0.1 6379
