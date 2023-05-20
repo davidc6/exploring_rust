aeb6f9cf82280259353bfaf22fa4dcec37e49213
David Chubabriya <david.chubabriya@gmail.com>
2023-05-20 22:18:46 +0100

    Comment Error type alias

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -21,5 +21,11 @@ pub use commands::Command;
 
 pub mod data_chunk;
 
+// any error that is safe to pass between threads (Send + Sync)
+// Send - safe to send to another thread
+// Sync - safe to share between threads (A type can be Sync only if it is Send)
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
+// we use type alias here to to aviod having to repeat
+// the Error type
+// For example, Result<bool> is interpreted as Result<bool, Error>
 pub type Result<T> = std::result::Result<T, Error>;
