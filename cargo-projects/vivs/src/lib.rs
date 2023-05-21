8055f120e8c500385ad474c9b36eb9ccd5b50fde
David Chubabriya <david.chubabriya@gmail.com>
2023-05-21 23:24:13 +0100

    Comment error type alias

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -22,10 +22,19 @@ pub use commands::Command;
 pub mod data_chunk;
 
 // any error that is safe to pass between threads (Send + Sync)
+//
 // Send - safe to send to another thread
 // Sync - safe to share between threads (A type can be Sync only if it is Send)
+//
+// Multiple trait bounds here are applied with a "+" in order
+// for Error to be bound by them
+//
+// <Box> returns reference to some memory on the heap,
+// since Error can be type is only known at runtime.
+//
+// dyn highlights the fact that calls to methods on the associated trait
+// (Error, in this case) are dynamically dispatched
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
-// we use type alias here to to aviod having to repeat
-// the Error type
+// we use type alias here to to aviod having to repeat the Error type
 // For example, Result<bool> is interpreted as Result<bool, Error>
 pub type Result<T> = std::result::Result<T, Error>;
