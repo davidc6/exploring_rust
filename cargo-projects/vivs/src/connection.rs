7624d7a4a5ae723ad4e3820de2be9ce1451ee3d2
David Chubabriya <david.chubabriya@gmail.com>
2023-05-23 20:11:56 +0100

    Clean up connection.rs module and return type

--- cargo-projects/vivs/src/connection.rs
+++ cargo-projects/vivs/src/connection.rs
@@ -58,37 +58,8 @@ impl Connection {
         // In this case self.buffer refers to the slice (full range) of the buffer (BytesMut)
         let mut cursored_buffer = Cursor::new(&self.buffer[..]);
 
-        let data_chunk_parsed = DataChunk::new(&mut cursored_buffer);
-
-        //     // iterate over the buffer and identify a end of a line
-        //     // then return the line
-        //     for position in current_position..end_position {
-        //         // we get the reference to the underlying value in the cursor
-        //         // if we find that at some point there's \r followed by \n then we have a line
-        //         // we then update position in the buffer to the start of the next line
-        //         // *1\r\n\x244\r\nPING\r\n\r\n - [*1\r\n] line 1, [x244\r\n] line 2, [PING\r\n\r\n] line 3
-        //         if cursored_buffer.get_ref()[position] == b'\r' && cursored_buffer.get_ref()[position + 1] == b'\n' {
-        //             cursored_buffer.set_position((position + 2) as u64);
-        //             // get usize by first converting slice in array
-        //             // then converting from native endian int value to usize
-        //             command_length = u64::from_be_bytes(
-        //                 cursored_buffer.get_ref()[current_position..position]
-        //                     .try_into()
-        //                     .unwrap(),
-        //             );
-        //             // return Ok(command_length);
-        //             break;
-        //             // cursored_buffer.get_ref()[current_position..position];
-        //         }
-        //     }
-
-        //     Ok(command_length)
-        //     }
-        //     _ => unimplemented!(),
-        // }
-
-        // Ok(data_chunk)
-        Ok(data_chunk_parsed.unwrap())
+        // Data frame parsed and structured
+        DataChunk::new(&mut cursored_buffer)
     }
 
     // Write chunk of data / frame to the stream
