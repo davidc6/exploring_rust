0de0cfc30c8183dc5977f8274125f13b33c89d0e
David Chubabriya <david.chubabriya@gmail.com>
2023-05-26 06:23:11 +0100

    Modified ping script to allow for messages

--- cargo-projects/vivs/scripts/ping.sh
+++ cargo-projects/vivs/scripts/ping.sh
@@ -4,4 +4,18 @@
 # - $4 - data type, number of chars in string
 # - PING - string which is the actual command
 
-printf '*1\r\n\x244\r\nPING\r\n' | nc -C -N 127.0.0.1 6379
+DATA_TYPE='\r\n\x244\r\n'
+
+# For now, any argument can be passed into the script to trigger this conditional
+if [ -z $1 ]; then 
+  COMMAND="*1${DATA_TYPE}PING\r\n"
+else 
+  COMMAND="*2${DATA_TYPE}PING\r\n\x243\r\nYES\r\n"
+fi
+
+# This is for debugging purposes, to see what we are sending to the server
+# use -e flag to process \r\n
+echo $COMMAND
+
+printf $COMMAND | nc -C -N 127.0.0.1 6379
+
