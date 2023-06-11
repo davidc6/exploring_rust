95c7682eb1487050ff2e075ee86abc062bdb1bf5
David Chubabriya <david.chubabriya@gmail.com>
2023-06-11 10:48:11 +0100

    Add Ping command

---
+++ cargo-projects/vivs/src/commands/ping.rs
@@ -0,0 +1,46 @@
+use bytes::{Buf, Bytes};
+
+use crate::{
+    data_chunk::{DataChunk, DataChunkFrame, Error},
+    Connection, Result,
+};
+
+use super::ParseError;
+
+#[derive(Debug, Default)]
+pub struct Ping {
+    message: Option<DataChunk>,
+}
+
+impl Ping {
+    pub fn new(message: Option<DataChunk>) -> Self {
+        Ping { message }
+    }
+
+    pub fn parse(mut data: DataChunkFrame) -> Result<Self> {
+        // TODO: peek instead of next so that we can
+        match data.next() {
+            Ok(data) => Ok(Ping::new(Some(data))),
+            Err(_) => Ok(Ping::default()),
+            // Err(e) => Err(e.into()),
+        }
+    }
+
+    pub async fn respond(self, conn: Connection) -> Result<()> {
+        let resp_val = "PONG";
+
+        if let Some(message) = self.message {
+            match message {
+                DataChunk::Bulk(data) => conn.write_chunk(data.chunk()).await?,
+                _ => conn.write_chunk(resp_val.as_bytes()).await?,
+            }
+
+            return Ok(());
+            // conn.write_chunk(message..as_bytes()).await?;
+        }
+
+        conn.write_chunk(resp_val.as_bytes()).await?;
+
+        Ok(())
+    }
+}
