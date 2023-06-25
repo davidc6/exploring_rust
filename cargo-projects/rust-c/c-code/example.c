3041328a017285e3d3692e3c3441f667147c18d9
David Chubabriya <david.chubabriya@gmail.com>
2023-06-25 21:17:02 +0100

    Add C code example

---
+++ cargo-projects/rust-c/c-code/example.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "../rust-code/include/bindings.h"
+
+// const char *something - non-modifiable (read-only) character data pointer
+// or pointer to constant char
+void update_done_cb(const char *prefix, const char *name)
+{
+    fprintf(stderr, "%s: %s\n", prefix, name);
+
+    // something other functionality can be added here as an example
+}
+
+void run(struct Person *person)
+{
+    // TODO: access struct member
+    // char prefix[] = "Original name: ";
+    // const char *l = person->last_name;
+    // update_done_cb(prefix, person->last_name);
+
+    person_cap_first_name(person, update_done_cb);
+
+    const char *last_name = "Brown";
+
+    person_update_last_name(person, last_name, update_done_cb);
+
+    // fprintf(stderr, "Hello %s \n", person->first_name);
+}
+
+int main(void)
+{
+    struct Person *person;
+    enum PersonStatus p = Person_new(
+        "test",
+        "Name",
+        &person);
+
+    run(person);
+}
