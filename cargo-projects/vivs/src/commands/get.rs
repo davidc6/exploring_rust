edae3d691415c5fb686291583aaadbb836aaced9
David Chubabriya <david.chubabriya@gmail.com>
2023-07-28 23:48:00 +0100

    Improve get command module

--- cargo-projects/vivs/src/commands/get.rs
+++ cargo-projects/vivs/src/commands/get.rs
@@ -2,6 +2,20 @@ use crate::data_chunk::{DataChunk, DataChunkFrame};
 use crate::Result;
 use crate::{Connection, DataStoreWrapper};
 use bytes::Buf;
+use std::fmt::Display;
+
+#[derive(Debug)]
+enum GetError {
+    NoKey,
+}
+
+impl std::error::Error for GetError {}
+
+impl Display for GetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
 
 pub struct Get {
     key: Option<DataChunk>,
@@ -10,23 +24,21 @@ pub struct Get {
 impl Get {
     pub fn parse(mut data: DataChunkFrame) -> Result<Self> {
         // TODO: peek instead of next so that we can
+        // TODO: should this even be a Result?
         match data.next() {
             Ok(data) => Ok(Get { key: Some(data) }),
-            Err(e) => Err(e.into()), 
+            Err(e) => Err(e.into()),
         }
     }
 
     pub async fn respond(self, conn: Connection, db: DataStoreWrapper) -> Result<()> {
-        match self.key.unwrap() {
+        let Some(key) = self.key else {
+            return Err(Box::new(GetError::NoKey));
+        };
+
+        match key {
             DataChunk::Bulk(key) => {
-                let key = std::str::from_utf8(key.chunk());
-
-                if key.is_err() {
-                    return Err("Failed to convert to UTF8".into());
-                }
-
-                let key = key.unwrap();
-
+                let key = std::str::from_utf8(key.chunk())?;
                 if key.is_empty() {
                     // TODO: extract various error prefix types
                     let error = "ERROR wrong number of arguments".to_owned();
@@ -39,7 +51,7 @@ impl Get {
 
                 // TODO: once TTL is figured out, check for expiration here
                 if let Some(value) = data_store_guard.db.get(key) {
-                    conn.write_chunk(super::DataType::Null, Some(value.as_bytes()))
+                    conn.write_chunk(super::DataType::SimpleString, Some(value.as_bytes()))
                         .await?
                 } else {
                     conn.write_chunk(super::DataType::Null, None).await?
