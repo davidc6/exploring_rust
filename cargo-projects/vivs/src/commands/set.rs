29dd19a79963e97cc8e868413c333a1e65579910
David Chubabriya <david.chubabriya@gmail.com>
2023-08-06 12:14:29 +0100

    Update SET command to use data chunk iterator

--- cargo-projects/vivs/src/commands/set.rs
+++ cargo-projects/vivs/src/commands/set.rs
@@ -1,13 +1,13 @@
-use std::fmt::Display;
-use bytes::Buf;
 use crate::{
     data_chunk::{DataChunk, DataChunkFrame},
     Connection, DataStoreWrapper, Error, Result,
 };
+use bytes::Buf;
+use std::fmt::Display;
 
 pub struct Set {
-    key: Option<DataChunk>,
-    value: Option<DataChunk>,
+    key: Option<String>,
+    value: Option<String>,
 }
 
 #[derive(Debug)]
@@ -16,89 +16,61 @@ pub enum CommandError {
     UnknownCommand,
 }
 
+#[derive(Debug)]
+pub enum SetError {
+    NoKey,
+    NoValue,
+}
+
+impl std::error::Error for SetError {}
 impl std::error::Error for CommandError {}
 
 impl Display for CommandError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "invalid first item to double")
+        write!(f, "Command error")
+    }
+}
+impl Display for SetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Set error")
     }
 }
 
 impl Set {
     pub fn parse(mut data: DataChunkFrame) -> Result<Self> {
-        // TODO: peek instead of next so that we can know if actual value exists
-
-        // TODO: a better way to parse values
-        // let length = data.len;
-        // if length < 2 {
-        //     return Err(Box::new(CommandError::NonParsableCommand));
-        // }
-        // let mut key = None;
-        // let mut value = None;
-        // for (position, data_chunk) in data.enumerate() {
-        //     match position {
-        //         0 => key = Some(data_chunk),
-        //         _ => value = Some(data_chunk),
-        //     }
-        // }
-        // Ok(Self { key, value })
-
-        match data.next() {
-            Ok(key) => match data.next() {
-                Ok(value) => Ok(Self {
-                    key: Some(key),
-                    value: Some(value),
-                }),
-                Err(e) => Err(e.into()),
-            },
-            Err(e) => Err(e.into()),
-        }
+        let Ok(key) = data.next_as_str() else {
+            return Err(Box::new(SetError::NoKey));
+        };
+        let Ok(value) = data.next_as_str() else {
+            return Err(Box::new(SetError::NoValue));
+        };
+        Ok(Self {
+            key: Some(key),
+            value: Some(value),
+        })
     }
 
     pub async fn respond(&self, connection: Connection, db: DataStoreWrapper) -> Result<()> {
-        let value_result = match self.value.as_ref().unwrap() {
-            DataChunk::Bulk(value) => {
-                let val = std::str::from_utf8(value.chunk());
-
-                val
-            }
-            _ => {
-                // TODO
-                panic!("Error");
-            }
-        };
-
-        match self.key.as_ref().unwrap() {
-            DataChunk::Bulk(key) => {
-                let key = std::str::from_utf8(key.chunk());
-
-                if key.is_err() {
-                    return Err("Failed to convert to UTF8".into());
-                }
-
-                let key = key.unwrap();
-
+        match self.key.clone() {
+            Some(key) => {
                 let mut data_store_guard = db.db.write().await;
 
                 if data_store_guard
                     .db
-                    .insert(key.to_owned(), value_result.unwrap().to_owned())
+                    .insert(key, self.value.clone().unwrap())
                     .is_none()
                 {
                     connection
                         .write_chunk(
                             super::DataType::SimpleString,
-                            Some(value_result.unwrap().to_owned().as_bytes()),
+                            Some(self.value.clone().unwrap().as_bytes()),
                         )
                         .await?
                 } else {
                     connection.write_chunk(super::DataType::Null, None).await?
                 }
             }
-            _ => {
-                // TODO
-                panic!("Error");
-            }
+            _ => unreachable!(),
         }
         Ok(())
     }
