e74de79b9fc9b741cb4415e65ccdf3ea958cffb4
David Chubabriya <david.chubabriya@gmail.com>
2023-09-09 20:18:43 +0100

    Add initial read_line implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Write};
+
+fn main() -> std::io::Result<()> {
+    let mut quit = false;
+
+    loop {
+        // write to stdout
+        write!(stdout(), "> ")?;
+        // flush everything ensuring all content reach destination
+        stdout().flush()?;
+
+        let mut buf = String::new();
+        // read a line of input and append to the buffer
+        // this line will wait for the "Enter" key to be pressed
+        stdin().read_line(&mut buf)?;
+
+        if quit {
+            break;
+        }
+
+        quit = true;
+    }
+
+    Ok(())
 }
