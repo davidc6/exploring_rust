4f66dd1fc775a2164f95627e5c6ddb621675efb8
David Chubabriya <david.chubabriya@gmail.com>
2023-09-22 22:15:13 +0100

    Improve comments

--- cargo-projects/vivs/src/server.rs
+++ cargo-projects/vivs/src/server.rs
@@ -5,8 +5,10 @@ use tokio::net::TcpListener;
 // New improved way of handling requests
 pub async fn start(addr: String, port: String) -> crate::Result<()> {
     let address = format!("{}:{}", addr, port);
+    // bind/asign address to the socket (ip address + port number)
     let tcp_listener = TcpListener::bind(&address).await?;
 
+    // listener construct, listens to incoming connections and assembles their processing
     let listener = Listener {
         tcp_listener,
         db: DataStoreWrapper::new(),
@@ -15,8 +17,6 @@ pub async fn start(addr: String, port: String) -> crate::Result<()> {
     // this should return a frame / bits of data
     // that will get parsed into commands
     listener.run().await?;
-
-    // TODO - command to do the work
 
     Ok(())
 }
