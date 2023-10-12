aa317d26af67ec461e54e6d796f40687799c7d24
David Chubabriya <david.chubabriya@gmail.com>
2023-10-12 23:17:01 +0100

    Init logger

--- cargo-projects/vivs/src/main.rs
+++ cargo-projects/vivs/src/main.rs
@@ -2,6 +2,8 @@ use vivs::server;
 
 #[tokio::main]
 pub async fn main() -> vivs::Result<()> {
+    env_logger::init();
+
     let addr = "127.0.0.1".to_string(); // TODO - extract into a config file
     let port = "6379".to_string();
 
