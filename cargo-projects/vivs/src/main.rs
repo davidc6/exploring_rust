54eb6465794e06b4fd3747184ebd985b3024ffc9
David Chubabriya <david.chubabriya@gmail.com>
2023-10-13 21:58:09 +0100

    Add logging to main.rs

--- cargo-projects/vivs/src/main.rs
+++ cargo-projects/vivs/src/main.rs
@@ -1,3 +1,4 @@
+use log::info;
 use vivs::server;
 
 #[tokio::main]
@@ -6,6 +7,8 @@ pub async fn main() -> vivs::Result<()> {
 
     let addr = "127.0.0.1".to_string(); // TODO - extract into a config file
     let port = "6379".to_string();
+
+    info!("Vivs is starting");
 
     server::start(addr, port).await?;
 
