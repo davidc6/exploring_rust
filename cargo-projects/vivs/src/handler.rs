1b59e3d534ad2b0fcc159781eed1838e1a88cabe
David Chubabriya <david.chubabriya@gmail.com>
2023-10-17 21:09:11 +0100

    Enable logging in handler.rs module

--- cargo-projects/vivs/src/handler.rs
+++ cargo-projects/vivs/src/handler.rs
@@ -1,4 +1,5 @@
 use crate::{Command, Connection, DataStoreWrapper, Result};
+use log::error;
 
 pub struct Handler {
     pub db: DataStoreWrapper,
@@ -10,7 +11,14 @@ impl Handler {
         // TODO: read a frame, should probably live in connection
         // read bits that host/client can send (frame)
         // this should return array of commands which will later parse
-        let payload = self.connection.read_and_process_stream().await?;
+        let payload = self
+            .connection
+            .read_and_process_stream()
+            .await
+            .map_err(|e| {
+                error!("Failed to processes the stream: {}", e);
+                e
+            })?;
 
         // TODO get the command interator
         // This will enable Command to get necessary data from the iterator by calling .next()
