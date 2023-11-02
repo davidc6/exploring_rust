cc5ef2e447fc43b94a42e964cdcf98ff4ee6dccc
David Chubabriya <david.chubabriya@gmail.com>
2023-11-02 21:16:30 +0000

    Fix PING message logging

--- cargo-projects/vivs/src/commands/ping.rs
+++ cargo-projects/vivs/src/commands/ping.rs
@@ -23,11 +23,15 @@ impl Ping {
     }
 
     pub async fn respond(self, conn: &mut Connection) -> Result<()> {
-        if let Some(message) = self.message {
+        if let Some(mut message) = self.message {
+            // This is also a hack similar to read_chunk_frame() in the connection.rs module
+            if message.chars().nth(0) != Some('\"') {
+                message = format!("\"{}\"", message);
+            }
             info!(
                 "{}",
                 format!(
-                    "{:?} {:?} {:?}",
+                    "{:?} {:?} {}",
                     conn.connected_peer_addr(),
                     PING_CMD,
                     message
@@ -47,6 +51,11 @@ impl Ping {
         Ok(())
     }
 
+    /// Pushes optional PING [message] to the segments array if it exists.
+    /// In order to do this, a default DataChunkFrame gets created which
+    /// takes is a command first and then the optional message.
+    /// This is a bit of a hack since DataChunkFrame and DataChunk are
+    /// different structs (even though potentially get could be one in the future).
     pub fn into_chunk(self) -> DataChunkFrame {
         let data_chunk_frame = DataChunkFrame::default();
         let cmd = format!("{}\r\n", PING_CMD);
