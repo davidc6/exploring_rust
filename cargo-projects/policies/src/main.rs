b824fc66db0ae2c63610323ffc63a5da934dc93c
David Chubabriya <david.chubabriya@gmail.com>
2024-03-14 13:11:08 +0000

    Add initial upsells functionality

--- cargo-projects/policies/src/main.rs
+++ cargo-projects/policies/src/main.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
 struct Policy {
     person_id: String,
     vehicle_id: String,
 }
 
 #[derive(Debug)]
-struct OwnedVehicles {
+struct OwnedVehicle {
     person_id: String,
     vehicle_id: String,
 }
@@ -14,32 +17,80 @@ struct UpsellOpportunity {
     vehicle_id: String,
 }
 
-fn get_owned_vehicles(person_ids: &[Policy]) -> Vec<OwnedVehicles> {
-    let owned_vehicles: Vec<OwnedVehicles> = vec![
-        OwnedVehicles {
+fn get_owned_vehicles(person_ids: Vec<&str>) -> Vec<OwnedVehicle> {
+    vec![
+        OwnedVehicle {
             person_id: "P1".to_owned(),
             vehicle_id: "V3".to_owned(),
         },
-        OwnedVehicles {
+        OwnedVehicle {
             person_id: "P1".to_owned(),
             vehicle_id: "V8".to_owned(),
         },
-        OwnedVehicles {
+        OwnedVehicle {
             person_id: "P2".to_owned(),
             vehicle_id: "V6".to_owned(),
         },
-    ];
-
-    owned_vehicles
+        OwnedVehicle {
+            person_id: "P2".to_owned(),
+            vehicle_id: "V11".to_owned(),
+        },
+    ]
 }
 
-fn find_potential_upsells(policies: Vec<Policy>) -> Vec<OwnedVehicles> {
-    let iter = policies.chunks(1);
+fn find_potential_upsells(policies: Vec<Policy>) -> Vec<OwnedVehicle> {
+    let mut upsells: Vec<OwnedVehicle> = vec![];
+    let mut user_vehicles: HashMap<&str, Vec<String>> = HashMap::new();
 
-    let mut upsells: Vec<OwnedVehicles> = vec![];
+    // build a map of user ids and owned vehicles
+    for policy in &policies {
+        if let Some(vehicle) = user_vehicles.get_mut(&policy.person_id as &str) {
+            vehicle.push(policy.vehicle_id.clone())
+        } else {
+            user_vehicles.insert(&policy.person_id, vec![policy.vehicle_id.clone()]);
+        }
+    }
 
-    for item in iter {
-        upsells.append(&mut get_owned_vehicles(item));
+    // chunk the vector
+    let policy_chunk = policies.chunks(1);
+
+    for policy in policy_chunk {
+        // iterator over a slice, map to return person_id field only and collect into a Vector of string slices
+        let user_ids: Vec<&str> = policy
+            .iter()
+            .map(|val| {
+                // let id: &str = val.person_id.as_ref();
+                // let owned_vehicles = user_vehicles.get_mut(id);
+
+                // if let Some(vehicles) = owned_vehicles {
+                //     vehicles.push(&val.vehicle_id);
+                // } else {
+                //     user_vehicles.insert(id, vec![&val.vehicle_id]);
+                // }
+
+                val.person_id.as_ref()
+            })
+            .collect();
+
+        let all_owned_vehicles = get_owned_vehicles(user_ids);
+
+        for vehicle in all_owned_vehicles {
+            // let OwnedVehicle {
+            //     person_id,
+            //     vehicle_id,
+            // } = vehicle;
+
+            let user_policies = user_vehicles.get_mut(&vehicle.person_id as &str).unwrap();
+
+            if !user_policies.contains(&vehicle.vehicle_id) {
+                upsells.push(OwnedVehicle {
+                    person_id: vehicle.person_id,
+                    vehicle_id: vehicle.vehicle_id.clone(),
+                });
+                // let v_c = vehicle_id.clone();
+                user_policies.push(vehicle.vehicle_id)
+            }
+        }
     }
 
     upsells
