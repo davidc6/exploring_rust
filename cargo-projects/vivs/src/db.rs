819bdf5dff537b21a0f9a3eb044232cb8c20f598
David Chubabriya <david.chubabriya@gmail.com>
2024-03-17 20:18:04 +0000

    Add expiration strategy comments

--- cargo-projects/vivs/src/db.rs
+++ cargo-projects/vivs/src/db.rs
@@ -1,6 +1,17 @@
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::RwLock;
 
+/// To enable expirations:
+///
+/// Option 1: key and value
+/// { [key]: { [value]: "hello", [ttl]: "16736377323" } }
+/// Potentially not all keys will have expiration set on them,
+/// so values that are None will cost more memory.
+///
+/// Option 2: 1) key and value Store AND 2) key and expiration Store
+/// { [key]: [value] } AND { [key]: [expiry] }
+/// We only store keys that have expiration set
+
 #[derive(Clone, Default)]
 pub struct DataStore {
     pub db: Arc<RwLock<HashMap<String, String>>>,
