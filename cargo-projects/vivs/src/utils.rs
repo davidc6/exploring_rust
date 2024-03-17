c0d16d7fa904a5795d8104be080b9322d98c71ba
David Chubabriya <david.chubabriya@gmail.com>
2024-03-17 21:29:38 +0000

    Add new error message

--- cargo-projects/vivs/src/utils.rs
+++ cargo-projects/vivs/src/utils.rs
@@ -4,3 +4,4 @@ pub fn unknown_cmd_err(value: String) -> String {
 
 pub const NO_CMD_ERR: &str = "No command supplied\r\n";
 pub const INCORRECT_ARGS_ERR: &str = "Incorrect number of arguments\r\n";
+pub const VALUE_NOT_INT_ERR: &str = "Value is not an integer\r\n";
