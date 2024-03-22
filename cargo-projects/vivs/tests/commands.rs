457771c4f4eb04d925ea7fe5fa9b8030c612bd08
David Chubabriya <david.chubabriya@gmail.com>
2024-03-22 21:43:46 +0000

    VIVS: Add TTL test

--- cargo-projects/vivs/tests/commands.rs
+++ cargo-projects/vivs/tests/commands.rs
@@ -2,7 +2,7 @@
 mod server {
     use std::net::SocketAddr;
     use tokio::{
-        io::{AsyncReadExt, AsyncWriteExt},
+        io::{self, AsyncReadExt, AsyncWriteExt},
         net::{TcpListener, TcpStream},
     };
     use vivs::{DataStore, Listener};
@@ -138,5 +138,52 @@ mod server {
         let _ = stream.read_exact(&mut buffer).await;
 
         assert_eq!(b"_\r\n", &buffer);
+    }
+
+    #[tokio::test]
+    async fn set_get_and_ttl_key() {
+        let addr = init_server().await;
+
+        let mut stream = TcpStream::connect(addr)
+            .await
+            .expect("Failed to open a TCP connection");
+
+        // SET
+        stream
+            .write_all(
+                b"*5\r\n$3\r\nSET\r\n$8\r\ngreeting\r\n$5\r\nhello\r\n$6\r\nexpire\r\n$2\r\n20\r\n",
+            )
+            .await
+            .unwrap();
+
+        let mut buffer = [0; 5];
+        let _ = stream.read_exact(&mut buffer).await;
+
+        assert_eq!(b"+OK\r\n", &buffer);
+
+        // GET
+        stream
+            .write_all(b"*2\r\n$3\r\nGET\r\n$8\r\ngreeting\r\n")
+            .await
+            .unwrap();
+
+        let mut buffer = [0; 8];
+        let _ = stream.read_exact(&mut buffer).await;
+
+        assert_eq!(b"+hello\r\n", &buffer);
+
+        // TTL
+        stream
+            .write_all(b"*2\r\n$3\r\nTTL\r\n$8\r\ngreeting\r\n")
+            .await
+            .unwrap();
+
+        let mut buffer = [0; 11];
+        let _ = stream.read_exact(&mut buffer).await;
+
+        // i.e. :19/r/n
+        let expected: [u8; 11] = [58, 19, 0, 0, 0, 0, 0, 0, 0, 13, 10];
+
+        assert_eq!(expected, buffer);
     }
 }
