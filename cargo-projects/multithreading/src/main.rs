1fe9d7791738b55069b14618144a0bf187864461
David Chubabriya <david.chubabriya@gmail.com>
2024-04-17 10:38:53 +0100

    Pull in rand crate

--- cargo-projects/multithreading/src/main.rs
+++ cargo-projects/multithreading/src/main.rs
@@ -1,13 +1,20 @@
-use crate::counter::most_frequent_char;
+use crate::{counter::most_frequent_char, flag::stop_flag};
 use counter::{increment_counter, increment_counter_atomic};
+use progress_updater::progress_updater;
 
 mod counter;
+mod flag;
+mod progress_updater;
 
 fn main() {
     // let s = String::from("hello, this is a message for the future");
     // let str_section = most_frequent_char(s, 6);
     // println!("{}", str_section);
 
-    increment_counter();
-    increment_counter_atomic();
+    // increment_counter();
+    // increment_counter_atomic();
+
+    // stop_flag();
+
+    progress_updater();
 }
