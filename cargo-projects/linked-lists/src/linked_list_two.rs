3f91492cc8c5c89df085737856d99da172b90f16
David Chubabriya <david.chubabriya@gmail.com>
2024-05-25 21:15:17 +0100

    Implement IterMut on LinkedList

--- cargo-projects/linked-lists/src/linked_list_two.rs
+++ cargo-projects/linked-lists/src/linked_list_two.rs
@@ -131,6 +131,30 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut ListNode<T>>,
+}
+
+// iter() return Iter which implements Iterator.
+impl<T> LinkedList<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next_elem.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod linked_list_two_tests {
     use super::*;
@@ -252,5 +276,21 @@ mod linked_list_two_tests {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn linked_list_iterator_iter_mut() {
+        let mut ll = LinkedList::new();
+
+        ll.push(1);
+        ll.push(2);
+        ll.push(3);
+
+        let mut iter_mut = ll.iter_mut();
+        assert_eq!(iter_mut.next(), Some(&mut 3));
+        assert_eq!(iter_mut.next(), Some(&mut 2));
+        assert_eq!(iter_mut.next(), Some(&mut 1));
+        assert_eq!(iter_mut.next(), None);
     }
 }
