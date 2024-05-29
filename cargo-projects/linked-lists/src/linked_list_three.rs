5f119253f553b86ad54ea1a7b955d1f70b11f59b
David Chubabriya <david.chubabriya@gmail.com>
2024-05-29 22:25:19 +0100

    Add prepend method to linked list

--- cargo-projects/linked-lists/src/linked_list_three.rs
+++ cargo-projects/linked-lists/src/linked_list_three.rs
@@ -1,12 +1,27 @@
 use std::rc::Rc;
 
-pub struct List<T> {
-    head: ListNodeConnection<T>,
+pub struct LinkedList<T> {
+    head: LinkedListNodeConnection<T>,
 }
 
-struct ListNode<T> {
-    val: T,
-    next_val: ListNodeConnection<T>,
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList { head: None }
+    }
+
+    pub fn prepend(&self, node: T) -> LinkedList<T> {
+        LinkedList {
+            head: Some(Rc::new(LinkedListNode {
+                current_node: node,
+                next_node: self.head.clone(),
+            })),
+        }
+    }
 }
 
-type ListNodeConnection<T> = Option<Rc<ListNode<T>>>;
+struct LinkedListNode<T> {
+    current_node: T,
+    next_node: LinkedListNodeConnection<T>,
+}
+
+type LinkedListNodeConnection<T> = Option<Rc<LinkedListNode<T>>>;
