b1c98939636b14e0ce06386605b94ed0ed264141
David Chubabriya <david.chubabriya@gmail.com>
2024-06-12 23:11:44 +0100

    Continue permutations implementation algo

--- cargo-projects/algorithms/src/permutation.rs
+++ cargo-projects/algorithms/src/permutation.rs
@@ -1,14 +1,29 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, fmt::Debug, hash::Hash};
 
-fn is_permutation<T: Eq + Hash>(vec_one: Vec<T>, vec_two: Vec<T>) -> bool {
+pub fn is_permutation<T: Debug + Eq + Hash>(vec_one: Vec<T>, vec_two: Vec<T>) -> bool {
     if vec_one.len() != vec_two.len() {
         return false;
     }
 
-    let mut chars_map = HashMap::new();
+    let mut char_count = HashMap::new();
 
     for character in vec_one {
-        *chars_map.entry(character).or_insert(1) += 1;
+        let _ = *char_count
+            .entry(character)
+            .and_modify(|val| *val += 1)
+            .or_insert(1);
+    }
+
+    for character in vec_two {
+        let entry = char_count.get_mut(&character);
+
+        if entry.is_none() || entry == Some(&mut 0) {
+            return false;
+        }
+
+        if let Some(val) = entry {
+            *val -= 1;
+        }
     }
 
     true
@@ -26,5 +41,35 @@ mod permutation_tests {
         let actual = is_permutation(vec_one, vec_two);
 
         assert!(!actual);
+    }
+
+    #[test]
+    fn vecs_are_not_permutations() {
+        let vec_one = vec!["a", "b", "c"];
+        let vec_two = vec!["a", "b", "d"];
+
+        let actual = is_permutation(vec_one, vec_two);
+
+        assert!(!actual);
+    }
+
+    #[test]
+    fn vec_contains_() {
+        let vec_one = vec!["a", "b", "c"];
+        let vec_two = vec!["b", "b", "a"];
+
+        let actual = is_permutation(vec_one, vec_two);
+
+        assert!(!actual);
+    }
+
+    #[test]
+    fn vec_pernutation() {
+        let vec_one = vec!["a", "b", "c"];
+        let vec_two = vec!["c", "b", "a"];
+
+        let actual = is_permutation(vec_one, vec_two);
+
+        assert_eq!(actual, true);
     }
 }
