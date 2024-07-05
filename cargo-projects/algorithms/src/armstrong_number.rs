2cd64dfb7ba423e37bba5b26a21517029b644f96
David Chubabriya <david.chubabriya@gmail.com>
2024-07-05 19:11:15 +0100

    Implement Armstrong number by converting to string

--- cargo-projects/algorithms/src/armstrong_number.rs
+++ cargo-projects/algorithms/src/armstrong_number.rs
@@ -1,4 +1,4 @@
-fn armstrong_number(num: u32) -> i32 {
+pub fn armstrong_number(num: u32) -> i32 {
     let mut current_ten = 10;
     let mut count = 1;
 
@@ -15,6 +15,27 @@ fn armstrong_number(num: u32) -> i32 {
     count += 1;
 
     count
+}
+
+pub fn armstrong_number_v2(num: u32) -> bool {
+    let num_as_str = num.to_string();
+    let len_of_str = num_as_str.len();
+    let mut sum = 0;
+
+    for character in num_as_str.chars() {
+        let n = character.to_digit(10).unwrap() as usize;
+        sum += n.pow(len_of_str as u32);
+
+        if sum > num as usize {
+            return false;
+        }
+    }
+
+    if sum == num as usize {
+        return true;
+    }
+
+    false
 }
 
 #[cfg(test)]
@@ -40,5 +61,17 @@ mod armstrong_number_tests {
         let actual = armstrong_number(145);
 
         assert_eq!(actual, 3);
+    }
+
+    #[test]
+    fn armstrong_number_v2_returns_true() {
+        let actual = armstrong_number_v2(153);
+        assert!(actual);
+    }
+
+    #[test]
+    fn armstrong_number_v2_returns_false() {
+        let actual = armstrong_number_v2(10);
+        assert!(!actual);
     }
 }
