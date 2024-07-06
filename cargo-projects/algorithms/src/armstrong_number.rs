bc55de0ec547fbf5ab45cb0db1f59bfbeeeaea72
David Chubabriya <david.chubabriya@gmail.com>
2024-07-06 20:18:15 +0100

    Implement Armstrong number without string conversion

--- cargo-projects/algorithms/src/armstrong_number.rs
+++ cargo-projects/algorithms/src/armstrong_number.rs
@@ -1,30 +1,39 @@
-pub fn armstrong_number(num: u32) -> i32 {
+pub fn armstrong_number(num: u32) -> bool {
     let mut current_ten = 10;
     let mut count = 1;
 
-    if num / current_ten == 0 {
-        return 1;
-    }
-
     while num / current_ten != 1 {
-        println!("{:?}", current_ten);
         current_ten *= 10;
         count += 1;
     }
 
     count += 1;
 
-    count
+    current_ten = 1;
+    let mut sum = 0;
+
+    for _ in 0..count {
+        current_ten *= 10;
+
+        let digit = ((num as f32 / current_ten as f32).fract() * 10.0) as u32;
+
+        sum += digit.pow(count as u32);
+    }
+
+    if sum == num {
+        return true;
+    }
+
+    false
 }
 
 pub fn armstrong_number_v2(num: u32) -> bool {
     let num_as_str = num.to_string();
-    let len_of_str = num_as_str.len();
-    let mut sum = 0;
+    let mut sum: usize = 0;
 
     for character in num_as_str.chars() {
         let n = character.to_digit(10).unwrap() as usize;
-        sum += n.pow(len_of_str as u32);
+        sum += n.pow(num_as_str.len() as u32);
 
         if sum > num as usize {
             return false;
@@ -44,24 +53,24 @@ mod armstrong_number_tests {
 
     #[test]
     fn armstrong_number_runs_with_single_digit() {
-        let actual = armstrong_number(1);
+        let actual = armstrong_number(153);
 
-        assert_eq!(actual, 1);
+        assert!(actual);
     }
 
     #[test]
     fn armstrong_number_runs_with_two_digits() {
-        let actual = armstrong_number(14);
+        let actual = armstrong_number(10);
 
-        assert_eq!(actual, 2);
+        assert!(!actual);
     }
 
-    #[test]
-    fn armstrong_number_runs_with_three_digits() {
-        let actual = armstrong_number(145);
+    // #[test]
+    // fn armstrong_number_runs_with_three_digits() {
+    //     let actual = armstrong_number(145);
 
-        assert_eq!(actual, 3);
-    }
+    //     assert_eq!(actual, 3);
+    // }
 
     #[test]
     fn armstrong_number_v2_returns_true() {
