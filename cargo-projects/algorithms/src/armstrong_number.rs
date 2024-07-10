19195c2b0903d9e1b1311bda314323821561892b
David Chubabriya <david.chubabriya@gmail.com>
2024-07-10 22:19:15 +0100

    Clean up armstrong algorithm

--- cargo-projects/algorithms/src/armstrong_number.rs
+++ cargo-projects/algorithms/src/armstrong_number.rs
@@ -1,46 +1,40 @@
-pub fn armstrong_number(num: u32) -> bool {
-    let mut current_ten = 10;
-    let mut count = 1;
+pub fn is_armstrong_number(num: u32) -> bool {
+    let mut base = 10;
+    let mut digit_count = 1;
 
-    while num / current_ten != 1 {
-        current_ten *= 10;
-        count += 1;
+    while num / base != 0 {
+        let r = base.checked_mul(10);
+
+        if r.is_none() {
+            break;
+        }
+
+        base = r.unwrap();
+        digit_count += 1;
     }
 
-    count += 1;
-
-    current_ten = 1;
+    base = 1;
     let mut sum = 0;
 
-    for _ in 0..count {
-        current_ten *= 10;
+    for i in 0..digit_count {
+        base *= 10;
 
-        let digit = ((num as f32 / current_ten as f32).fract() * 10.0) as u32;
+        let digit = ((num as f64 / base as f64).fract() * 10.0) as u32;
 
-        sum += digit.pow(count as u32);
+        println!(
+            "NUM {:?} {} {:?}",
+            i,
+            // ((num as f64 / base as f64).fract() * 10.0),
+            num / 10,
+            digit
+        );
+
+        // base /= 10;
+
+        sum += digit.pow(digit_count as u32);
     }
 
     if sum == num {
-        return true;
-    }
-
-    false
-}
-
-pub fn armstrong_number_v2(num: u32) -> bool {
-    let num_as_str = num.to_string();
-    let mut sum: usize = 0;
-
-    for character in num_as_str.chars() {
-        let n = character.to_digit(10).unwrap() as usize;
-        sum += n.pow(num_as_str.len() as u32);
-
-        if sum > num as usize {
-            return false;
-        }
-    }
-
-    if sum == num as usize {
         return true;
     }
 
@@ -52,35 +46,67 @@ mod armstrong_number_tests {
     use super::*;
 
     #[test]
-    fn armstrong_number_runs_with_single_digit() {
-        let actual = armstrong_number(153);
-
-        assert!(actual);
+    fn zero_is_an_armstrong_number() {
+        assert!(is_armstrong_number(0))
     }
 
     #[test]
-    fn armstrong_number_runs_with_two_digits() {
-        let actual = armstrong_number(10);
-
-        assert!(!actual);
-    }
-
-    // #[test]
-    // fn armstrong_number_runs_with_three_digits() {
-    //     let actual = armstrong_number(145);
-
-    //     assert_eq!(actual, 3);
-    // }
-
-    #[test]
-    fn armstrong_number_v2_returns_true() {
-        let actual = armstrong_number_v2(153);
-        assert!(actual);
+    fn single_digit_numbers_are_armstrong_numbers() {
+        assert!(is_armstrong_number(5))
     }
 
     #[test]
-    fn armstrong_number_v2_returns_false() {
-        let actual = armstrong_number_v2(10);
-        assert!(!actual);
+    fn there_are_no_2_digit_armstrong_numbers() {
+        assert!(!is_armstrong_number(10))
+    }
+
+    #[test]
+    fn three_digit_armstrong_number() {
+        assert!(is_armstrong_number(153))
+    }
+
+    #[test]
+    fn three_digit_non_armstrong_number() {
+        assert!(!is_armstrong_number(100))
+    }
+
+    #[test]
+    fn four_digit_armstrong_number() {
+        assert!(is_armstrong_number(9474))
+    }
+
+    #[test]
+    fn four_digit_non_armstrong_number() {
+        assert!(!is_armstrong_number(9475))
+    }
+
+    #[test]
+    fn seven_digit_armstrong_number() {
+        assert!(is_armstrong_number(9_926_315))
+    }
+
+    #[test]
+    fn seven_digit_non_armstrong_number() {
+        assert!(!is_armstrong_number(9_926_316))
+    }
+
+    #[test]
+    fn nine_digit_armstrong_number() {
+        assert!(is_armstrong_number(912_985_153));
+    }
+
+    #[test]
+    fn nine_digit_non_armstrong_number() {
+        assert!(!is_armstrong_number(999_999_999));
+    }
+
+    #[test]
+    fn ten_digit_non_armstrong_number() {
+        assert!(!is_armstrong_number(3_999_999_999));
+    }
+
+    #[test]
+    fn properly_handles_overflow() {
+        assert!(!is_armstrong_number(4_106_098_957));
     }
 }
