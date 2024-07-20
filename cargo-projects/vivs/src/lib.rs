03a61c3f1fe67fcc7e6f0e8b2f7c17ff86b3a400
David Chubabriya <david.chubabriya@gmail.com>
2024-07-20 22:04:19 +0100

    [Vivs] add sample client code

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -1,5 +1,6 @@
 // include the code found in src/listener.rs
 pub mod listener;
+use data_chunk::DataChunk;
 // use declarations are private to the containing module
 // hence we use pub keyword to re-export a name
 pub use listener::Listener;
@@ -17,6 +18,7 @@ pub use connection::Connection;
 
 pub mod commands;
 pub use commands::Command;
+use tokio::net::TcpStream;
 
 pub mod data_chunk;
 
@@ -49,3 +51,43 @@ pub type GenericResult<T> = std::result::Result<T, GenericError>;
 
 // This is the default port the server listens on
 pub const PORT: u16 = 9000;
+
+pub struct Client {
+    connection: Connection,
+}
+
+impl Client {
+    pub async fn new() -> Result<Self, core::fmt::Error> {
+        let address = format!("127.0.0.1:{}", PORT);
+        let stream = TcpStream::connect(address).await;
+        let connection = Connection::new(stream.unwrap());
+        Ok(Client { connection })
+    }
+
+    pub async fn get(&mut self, value: String) -> Option<String> {
+        // e.g. *2\r\n$3\r\GET\r\n$4\r\nMary\r\n
+        let frame = format!("*2\r\n$3\r\nGET\r\n${}\r\n{}\r\n", value.len(), value);
+        let _ = self.connection.write_complete_frame(&frame).await;
+        let processed_stream = self.connection.read_and_process_stream().await;
+        match processed_stream.unwrap().next() {
+            Some(DataChunk::Null) => None,
+            Some(DataChunk::Bulk(b)) => Some(std::str::from_utf8(&b).unwrap().to_owned()),
+            _ => None,
+        }
+    }
+
+    pub async fn set(&mut self, key: String, value: String) -> String {
+        // e.g. *3\r\n$3\r\nSET\r\n$4\r\nname\r\n$4\r\nMary\r\n
+        let frame = format!(
+            "*3\r\n$3\r\nSET\r\n${}\r\n{}\r\n${}\r\n{}\r\n",
+            key.len(),
+            key,
+            value.len(),
+            value
+        );
+        let _ = self.connection.write_complete_frame(&frame).await;
+        let frame = self.connection.read_chunk_frame().await.unwrap();
+        let frame_utf8 = std::str::from_utf8(&frame);
+        frame_utf8.unwrap().to_owned()
+    }
+}
