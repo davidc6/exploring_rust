5e2dff050a92c201693922e609db9d89b2ed9d88
David Chubabriya <david.chubabriya@gmail.com>
2024-07-25 23:09:11 +0100

    [Vivs] Clean up example client implementation

--- cargo-projects/vivs/src/lib.rs
+++ cargo-projects/vivs/src/lib.rs
@@ -103,7 +103,7 @@ impl Client {
         let _ = self.connection.write_complete_frame(&frame).await;
         let mut frame = self.connection.read_and_process_stream().await.unwrap();
 
-        let frame_utf8 = match frame.next() {
+        match frame.next() {
             Some(DataChunk::Null) => "0".to_owned(),
             Some(DataChunk::Integer(val)) => {
                 // convert Bytes to bytes array
@@ -113,17 +113,15 @@ impl Client {
 
                 // converts the slice to an array of u8 elements (since u64 is 8 bytes)
                 let arr_u8: [u8; 8] = bytes_slice[0..8].try_into().unwrap();
-                let integer_as_string = if cfg!(target_endian = "big") {
+
+                if cfg!(target_endian = "big") {
                     u64::from_be_bytes(arr_u8)
                 } else {
                     u64::from_le_bytes(arr_u8)
                 }
-                .to_string();
-                integer_as_string
+                .to_string()
             }
             _ => "0".to_owned(),
-        };
-
-        frame_utf8
+        }
     }
 }
