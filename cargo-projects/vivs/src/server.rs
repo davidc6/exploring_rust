1ead025de6bf32d354ecaeaa2b863bb694d74467
David Chubabriya <david.chubabriya@gmail.com>
2024-08-24 23:12:20 +0100

    (chore): [VIVS] server uses global config

--- cargo-projects/vivs/src/server.rs
+++ cargo-projects/vivs/src/server.rs
@@ -1,9 +1,15 @@
-use crate::{DataStore, GenericResult, Listener};
+use crate::{DataStore, GenericResult, Listener, VIVS_CONFIG};
 use log::{error, info};
 use tokio::net::TcpListener;
 
-pub async fn start(ipv4: &str, port: u16) -> GenericResult<()> {
-    let address = format!("{}:{}", ipv4, port);
+pub async fn start() -> GenericResult<()> {
+    let vivs_config_ref = VIVS_CONFIG.with(|arc| arc.clone());
+    let vivs_config = vivs_config_ref.lock();
+
+    let v = vivs_config.await;
+    let port = v.get("port").unwrap();
+
+    let address = format!("{}:{}", v.get(&"address".to_owned()).unwrap(), port);
     info!("Attempting to bind on port {port}");
 
     // Bind/assign the address to the socket (ip address + port number)
