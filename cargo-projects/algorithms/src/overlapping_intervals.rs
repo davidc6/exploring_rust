f6422b25058cb0d56c0a9ad68f70d607e3ff0cba
David Chubabriya <david.chubabriya@gmail.com>
2024-11-23 12:56:45 +0000

    feat(data_structures): add another test to overlapping intervals merge solution

--- cargo-projects/algorithms/src/overlapping_intervals.rs
+++ cargo-projects/algorithms/src/overlapping_intervals.rs
@@ -1,25 +1,30 @@
 type Merge = Vec<Vec<i32>>;
 
 fn merge_1(mut input: Merge) -> Merge {
+    // First, sort the vector by end value
     input.sort_by(|a, b| a.get(1).unwrap().cmp(b.get(1).unwrap()));
 
     let mut output = vec![];
 
+    // Iterate over the vector
     for interval in input {
+        // Push initial vector
         if output.is_empty() {
             output.push(interval);
             continue;
         }
 
+        // Get last vector in the new output vector
         let last = output.last_mut().unwrap();
 
+        // Check if end is NOT in range of the current iteration vector,
+        // if it is not then push as new vector into the output vector
         if !(interval.first().unwrap()..interval.get(1).unwrap()).contains(&last.get(1).unwrap()) {
-            println!("No contain {:?} {:?}", interval, last.get(1).unwrap());
             output.push(interval);
             continue;
         }
 
-        // contains interval
+        // End is in range, so determine if we need to swap the start value
         let i = *interval.first().unwrap();
         last[0] = if i < last[0] { i } else { last[0] };
         last[1] = *interval.get(1).unwrap();
@@ -36,6 +41,14 @@ mod intervals_test {
     fn merge_1_works() {
         let input = vec![vec![2, 6], vec![1, 3], vec![15, 18], vec![8, 10]];
         let expected = vec![vec![1, 6], vec![8, 10], vec![15, 18]];
+        let output = merge_1(input);
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn merge_2_works() {
+        let input = vec![vec![1, 4], vec![4, 5]];
+        let expected = vec![vec![1, 5]];
         let output = merge_1(input);
         assert_eq!(expected, output);
     }
