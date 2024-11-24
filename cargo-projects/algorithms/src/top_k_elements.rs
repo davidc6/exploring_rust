5fc23cf87983526332678e4b133dcbcad7bcd38c
David Chubabriya <david.chubabriya@gmail.com>
2024-11-24 19:25:25 +0000

    feat(data_structures): update max heap implementation

--- cargo-projects/algorithms/src/top_k_elements.rs
+++ cargo-projects/algorithms/src/top_k_elements.rs
@@ -3,7 +3,7 @@
 //! Sorting vectors/arrays takes O(n log n).
 //! Using min or max heap takes O(n log k), where k
 //!
-//! k largest - min-heap
+//! k largest  - min-heap
 //! k smallest - max-heap
 //!
 
@@ -21,6 +21,42 @@ fn top_1(input: Vec<i32>, k: u32) -> Vec<i32> {
     output
 }
 
+struct Node {
+    val: i32,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
+}
+
+fn top_2(mut input: Vec<i32>, k: u32) -> Vec<i32> {
+    let mut m_h = vec![];
+
+    for val in input {
+        m_h.push(val);
+        if m_h.len() == 1 {
+            continue;
+        }
+        let l = m_h.len() - 1;
+        top_2_r(&mut m_h, l);
+    }
+
+    m_h
+}
+
+fn get_parent(index: usize) -> usize {
+    index / 2
+}
+
+fn top_2_r(min_heap: &mut [i32], index: usize) {
+    let parent_index = get_parent(index);
+    let min_heap_length = index;
+
+    if min_heap[parent_index] > min_heap[min_heap_length] {
+        min_heap.swap(parent_index, min_heap_length);
+
+        top_2_r(min_heap, parent_index);
+    }
+}
+
 #[cfg(test)]
 mod top_k_elements_tests {
     use super::*;
@@ -31,5 +67,13 @@ mod top_k_elements_tests {
         let result = top_1(v, 3);
 
         assert_eq!(result, vec![11, 9, 7]);
+    }
+
+    #[test]
+    fn top_2_works() {
+        let v = vec![10, 8, 15, 3];
+        let output = top_2(v, 1);
+
+        assert_eq!(output, vec![3, 8, 10, 15]);
     }
 }
