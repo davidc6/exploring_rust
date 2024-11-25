7ef8b2496586ab7e6ddb483ab204b18e6fdc79e7
David Chubabriya <david.chubabriya@gmail.com>
2024-11-25 20:42:36 +0000

    feat(data_structures): clean up top k elements algorithm

--- cargo-projects/algorithms/src/top_k_elements.rs
+++ cargo-projects/algorithms/src/top_k_elements.rs
@@ -5,9 +5,8 @@
 //!
 //! k largest  - min-heap
 //! k smallest - max-heap
-//!
 
-use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::collections::{BinaryHeap, HashMap};
 
 fn top_1(input: Vec<i32>, k: u32) -> Vec<i32> {
     let mut b_h = BinaryHeap::from(input);
@@ -21,7 +20,7 @@ fn top_1(input: Vec<i32>, k: u32) -> Vec<i32> {
     output
 }
 
-fn top_2(mut input: Vec<i32>, k: u32) -> Vec<i32> {
+fn top_2(input: Vec<i32>, k: u32) -> Vec<i32> {
     let mut m_h = vec![];
 
     for val in input {
@@ -70,8 +69,8 @@ impl Ord for MostFrequentFirst {
 }
 
 // Leetcode 347, Top K Frequent Elements
-fn top_3(v: Vec<i32>, k: i32) -> Vec<i32> {
-    let frequencies = v.iter().copied().fold(HashMap::new(), |mut map, val| {
+fn top_3(input: Vec<i32>, k: i32) -> Vec<i32> {
+    let frequencies = input.iter().copied().fold(HashMap::new(), |mut map, val| {
         map.entry(val)
             .and_modify(|frequency| *frequency += 1)
             .or_insert(1);
@@ -85,18 +84,18 @@ fn top_3(v: Vec<i32>, k: i32) -> Vec<i32> {
         if heap.len() < k as usize {
             heap.push(MostFrequentFirst(Count(-freq, key)));
         } else {
+            // We want to make sure that no more than k elements are kept on the heap,
+            // hence we push and pop here
             heap.push(MostFrequentFirst(Count(-freq, key)));
             heap.pop();
         }
     }
 
-    let sorted = heap.into_sorted_vec();
-    let a = &sorted[(sorted.len() - k as usize)..sorted.len()];
-    let v = Vec::from(a);
-
-    let mut ans: Vec<i32> = v.iter().map(|val| val.0 .1).collect();
-    ans.reverse();
-    ans
+    let sorted_vec = heap.into_sorted_vec();
+    Vec::from(&sorted_vec[(sorted_vec.len() - k as usize)..sorted_vec.len()])
+        .iter()
+        .map(|val| val.0 .1)
+        .collect()
 }
 
 #[cfg(test)]
@@ -124,6 +123,6 @@ mod top_k_elements_tests {
         let v = vec![1, 1, 1, 2, 2, 3, 5, 5, 5, 5, 5];
         let c = top_3(v, 2);
 
-        assert_eq!(c, vec![1, 5]);
+        assert_eq!(c, vec![5, 1]);
     }
 }
