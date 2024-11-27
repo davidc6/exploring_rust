3d4026986c45b00fdd0804433d72d413513c0182
David Chubabriya <david.chubabriya@gmail.com>
2024-11-27 21:35:00 +0000

    feat(data_structures): add iteratir 2 algo

--- cargo-projects/algorithms/src/iterators.rs
+++ cargo-projects/algorithms/src/iterators.rs
@@ -1,7 +1,16 @@
 fn iter_1(arr: &[i32]) -> Vec<i32> {
-    let a = arr.to_owned();
-    let a = a.iter().filter(|&a| *a > 10).copied().collect();
-    a
+    let vec_owned = arr.to_owned();
+    vec_owned.iter().filter(|&a| *a > 10).copied().collect()
+}
+
+fn iter_2(arr: &mut [i32]) -> &mut [i32] {
+    for element in arr.iter_mut() {
+        if *element > 10 {
+            *element *= 10;
+        }
+    }
+
+    arr
 }
 
 #[cfg(test)]
@@ -14,6 +23,16 @@ mod iterators_tests {
 
         let actual = iter_1(&ints);
         let expected = vec![20, 13];
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn iter_2_works() {
+        let mut ints = [20, 13, 7, 6, 8];
+
+        let actual = iter_2(&mut ints);
+        let expected = vec![200, 130, 7, 6, 8];
 
         assert_eq!(actual, expected);
     }
