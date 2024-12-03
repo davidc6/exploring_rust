d8bc40ee27b4aa90f8d0202b19fa3e8070b36564
David Chubabriya <david.chubabriya@gmail.com>
2024-12-03 23:31:48 +0000

    feat(data_structures): add decode string function

--- cargo-projects/algorithms/src/strings.rs
+++ cargo-projects/algorithms/src/strings.rs
@@ -33,7 +33,7 @@ fn encode_decode_strings(strs: &mut [&str]) -> String {
 
     for value in strs {
         let length = value.len();
-        let formatted_str = format!("${length}{value}");
+        let formatted_str = format!("{length}${value}");
 
         result.push_str(&formatted_str);
     }
@@ -41,6 +41,42 @@ fn encode_decode_strings(strs: &mut [&str]) -> String {
     result
 }
 
+fn decode_strings(str: &str) -> Vec<String> {
+    let mut count = "".to_string();
+    let mut count_i = 0;
+    let mut strs = vec![];
+    let mut cur_str = "".to_string();
+
+    let mut on_str = false;
+
+    for char in str.chars() {
+        if on_str && count_i > 0 {
+            cur_str.push_str(&char.to_string());
+            count_i -= 1;
+            continue;
+        }
+
+        if on_str && count_i == 0 {
+            strs.push(cur_str.to_owned());
+            on_str = false;
+            count_i = 0;
+            count.clear();
+            cur_str.clear();
+        }
+
+        if char == '$' {
+            count_i = count.parse().unwrap();
+            on_str = true;
+            continue;
+        } else {
+            count.push_str(&char.to_string());
+        }
+    }
+
+    strs.push(cur_str.to_owned());
+
+    strs
+}
 #[cfg(test)]
 mod string_tests {
     use super::*;
@@ -73,9 +109,18 @@ mod string_tests {
     }
 
     #[test]
-    fn encode_decode_strings_works() {
+    fn encode_strings_works() {
         let mut strs = ["hello", "hi", "hey"];
         let actual = encode_decode_strings(&mut strs);
-        assert_eq!(actual, "$5hello$2hi$3hey".to_owned());
+        assert_eq!(actual, "5$hello2$hi3$hey".to_owned());
+    }
+
+    #[test]
+    fn decode_strings_works() {
+        let str = "5$hello2$hi3$hey";
+        let expected = vec!["hello".to_string(), "hi".to_string(), "hey".to_string()];
+        let actual = decode_strings(str);
+
+        assert_eq!(expected, actual);
     }
 }
