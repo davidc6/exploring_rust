55b953f57a09c5dd114369e09653ecc7b00bc025
David Chubabriya <david.chubabriya@gmail.com>
2025-02-01 22:44:14 +0000

    feat(pledge-2): add pageallocator struct

--- cargo-projects/pledge/src/lib.rs
+++ cargo-projects/pledge/src/lib.rs
@@ -1,3 +1,8 @@
+//! This is very simple (and so far not quite efficient) memory allocator.
+//! It maps entries pages for every allocation.
+//! There are many ways to make it faster, examples:
+//! TODO
+
 use core::cmp::max;
 use std::{
     alloc::{GlobalAlloc, Layout},
@@ -9,33 +14,52 @@ use std::{
 // hence initialized lazily (when accessed) once
 static PAGE_SIZE: LazyLock<usize> = LazyLock::new(page_size);
 
+// We need to get the OS page size in order to create
 fn page_size() -> usize {
+    // Get the size of page. A page is a contiguous block of memory
     unsafe { libc::sysconf(libc::_SC_PAGESIZE) as usize }
 }
 
-// unsafe impl GlobalAlloc for
+#[derive(Default)]
+pub struct PageAllocator {}
 
-// Returning raw unsafe pointer here
-unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-    // TODO
-    let aligned_layout = match layout.align_to(max(layout.align(), *PAGE_SIZE)) {
-        Ok(l) => l.pad_to_align(),
-        Err(_) => return ptr::null_mut(),
-    };
+unsafe impl GlobalAlloc for PageAllocator {
+    // Layout - describes a layout of memory.
+    // Returning raw unsafe pointer
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // Helps to ensure that raw pointer is correctly aligned to a specified alignment boundary.
+        //
+        let aligned_layout = match layout.align_to(max(layout.align(), *PAGE_SIZE)) {
+            Ok(l) => l.pad_to_align(),
+            Err(_) => return ptr::null_mut(),
+        };
 
-    // for Unix-like systems only
-    // mmap - creates a new mapping in the
-    // virtual address of the calling process
-    let address = libc::mmap(
-        ptr::null_mut(),
-        aligned_layout.size(),
-        libc::PROT_READ | libc::PROT_WRITE,
-        libc::MAP_PRIVATE | libc::MAP_ANONYMOUS,
-        -1,
-        0,
-    );
+        // for Unix-like systems only
+        // mmap - creates a new mapping in the
+        // virtual address of the calling process.
+        // We pass:
+        //   1) null mutable raw pointer (zero initializing a pointer, the resulting address is 0.
+        //   2) minimum size for the memory block of this size (in bytes)
+        //   3) read and write flags to be able to read and write to
+        //   4) the memory is private to process and does not represent a file stored in memory.
+        //   5) not a file in memory
+        let address = libc::mmap(
+            ptr::null_mut(),
+            aligned_layout.size(),
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_PRIVATE | libc::MAP_ANONYMOUS,
+            -1,
+            0,
+        );
 
-    address as _
+        address as _
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        if let Ok(aligned) = layout.align_to(max(layout.align(), *PAGE_SIZE)) {
+            libc::munmap(ptr as _, aligned.pad_to_align().size());
+        }
+    }
 }
 
 #[cfg(test)]
