16db81787e72ab50010f4c174f794da6bb159014
David Chubabriya <david.chubabriya@gmail.com>
2025-03-16 21:07:45 +0000

    feat(rate-limiter): swap mut for interior mutability

--- cargo-projects/rate-limiter/src/lib.rs
+++ cargo-projects/rate-limiter/src/lib.rs
@@ -1,40 +1,42 @@
 #[cfg(test)]
 use mock_instant::thread_local::SystemTime;
-use std::time::Duration;
 #[cfg(not(test))]
 use std::time::SystemTime;
+use std::{
+    cell::{Cell, RefCell},
+    time::Duration,
+};
 
 #[derive(Debug)]
 pub struct RateLimiter {
     pub quota: u16,
-    pub current_timestamp: SystemTime,
-    current_quota: u16,
+    pub current_timestamp: RefCell<SystemTime>,
+    current_quota: Cell<u16>,
 }
 
 impl RateLimiter {
     pub fn new(actions_per_second: u16) -> Self {
         RateLimiter {
             quota: actions_per_second,
-            current_quota: 0,
-            current_timestamp: SystemTime::now(),
+            current_quota: Cell::new(0),
+            current_timestamp: RefCell::new(SystemTime::now()),
         }
     }
 
-    pub fn is_quota_exceeded(&mut self) -> bool {
-        let elapsed_time = self.current_timestamp.elapsed().unwrap();
+    pub fn is_quota_exceeded(&self) -> bool {
+        let elapsed_time = self.current_timestamp.borrow().elapsed().unwrap();
         let one_second = Duration::from_millis(1000);
 
-        self.current_quota += 1;
+        self.current_quota
+            .swap(&Cell::new(self.current_quota.take() + 1));
 
         if elapsed_time >= one_second {
-            // let diff = elapsed_time - one_second;
-            // println!("{:?}", diff.as_millis());
-            self.current_timestamp = SystemTime::now();
-            self.current_quota = 1;
+            self.current_timestamp.replace(SystemTime::now());
+            self.current_quota.swap(&Cell::new(1));
             return false;
         }
 
-        self.current_quota == self.quota
+        self.current_quota.get() == self.quota
     }
 }
 
@@ -44,13 +46,13 @@ mod tests {
 
     #[test]
     fn initialisation_works() {
-        let mut rate_limiter = RateLimiter::new(10);
+        let rate_limiter = RateLimiter::new(10);
         assert!(!rate_limiter.is_quota_exceeded());
     }
 
     #[test]
     fn quota_exceed_works() {
-        let mut rate_limiter = RateLimiter::new(10);
+        let rate_limiter = RateLimiter::new(10);
 
         for _ in 0..9 {
             assert!(!rate_limiter.is_quota_exceeded());
@@ -62,14 +64,14 @@ mod tests {
     #[test]
     fn quota_reset_works() {
         let actions_per_second = 10;
-        let mut rate_limiter = RateLimiter::new(actions_per_second);
+        let rate_limiter = RateLimiter::new(actions_per_second);
 
         // Call the method 9 times
         for _ in 0..actions_per_second - 1 {
             assert!(!rate_limiter.is_quota_exceeded());
         }
 
-        assert!(rate_limiter.current_quota == 9);
+        assert!(rate_limiter.current_quota.get() == 9);
         // Quota should not have been exceeded yet since we only called it 9 times (quota is 10)
         assert!(rate_limiter.is_quota_exceeded());
 
@@ -78,7 +80,6 @@ mod tests {
 
         // Quota has been reset
         assert!(!rate_limiter.is_quota_exceeded());
-
-        assert!(rate_limiter.current_quota == 1);
+        assert!(rate_limiter.current_quota.get() == 1);
     }
 }
