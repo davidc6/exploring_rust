741b0ed6598ab97a72de3066f5a4fef9795456ae
David Chubabriya <david.chubabriya@gmail.com>
2025-03-16 15:19:57 +0000

    feat(rate-limiter): remove test dependency on features

--- cargo-projects/rate-limiter/src/lib.rs
+++ cargo-projects/rate-limiter/src/lib.rs
@@ -1,13 +1,13 @@
-#[cfg(feature = "mock-instant")]
+#[cfg(test)]
 use mock_instant::thread_local::SystemTime;
 use std::time::Duration;
-#[cfg(not(feature = "mock-instant"))]
+#[cfg(not(test))]
 use std::time::SystemTime;
 
 #[derive(Debug)]
 pub struct RateLimiter {
-    quota: u16,
-    current_timestamp: SystemTime,
+    pub quota: u16,
+    pub current_timestamp: SystemTime,
     current_quota: u16,
 }
 
@@ -24,18 +24,17 @@ impl RateLimiter {
         let elapsed_time = self.current_timestamp.elapsed().unwrap();
         let one_second = Duration::from_millis(1000);
 
+        self.current_quota += 1;
+
         if elapsed_time >= one_second {
             // let diff = elapsed_time - one_second;
             // println!("{:?}", diff.as_millis());
             self.current_timestamp = SystemTime::now();
             self.current_quota = 1;
-            false
-        } else if self.current_quota == self.quota {
-            true
-        } else {
-            self.current_quota += 1;
-            false
+            return false;
         }
+
+        self.current_quota == self.quota
     }
 }
 
@@ -53,7 +52,7 @@ mod tests {
     fn quota_exceed_works() {
         let mut rate_limiter = RateLimiter::new(10);
 
-        for _ in 0..10 {
+        for _ in 0..9 {
             assert!(!rate_limiter.is_quota_exceeded());
         }
 
@@ -61,20 +60,25 @@ mod tests {
     }
 
     #[test]
-    #[cfg(feature = "mock-instant")]
     fn quota_reset_works() {
         let actions_per_second = 10;
         let mut rate_limiter = RateLimiter::new(actions_per_second);
 
+        // Call the method 9 times
         for _ in 0..actions_per_second - 1 {
             assert!(!rate_limiter.is_quota_exceeded());
         }
 
         assert!(rate_limiter.current_quota == 9);
-        assert!(!rate_limiter.is_quota_exceeded());
+        // Quota should not have been exceeded yet since we only called it 9 times (quota is 10)
+        assert!(rate_limiter.is_quota_exceeded());
 
+        // Progress by 1s
         mock_instant::thread_local::MockClock::advance_system_time(Duration::from_millis(1000));
 
+        // Quota has been reset
         assert!(!rate_limiter.is_quota_exceeded());
+
+        assert!(rate_limiter.current_quota == 1);
     }
 }
