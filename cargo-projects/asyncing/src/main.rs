d3f446cb6807f9f78c414d3af61f13a067da73bb
David Chubabriya <david.chubabriya@gmail.com>
2025-04-04 22:48:29 +0000

    feat(asynching): add proper routing

--- cargo-projects/asyncing/src/main.rs
+++ cargo-projects/asyncing/src/main.rs
@@ -1,4 +1,14 @@
-use axum::{routing::get, serve::Listener, Router};
+use std::fmt;
+
+use axum::{
+    body::Body,
+    debug_handler,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    serve::Listener,
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -10,20 +20,69 @@ pub enum ApiError {
     JsonParserError(reqwest::Error),
 }
 
-const BOOKS_URL: &str = "https://raw.githubusercontent.com/davidc6/exploring_rust/refs/heads/main/cargo-projects/asyncing/src/mock-data/books.json";
+const BOOKS_URL: &str = "https://raw.githubusercontent.co/davidc6/exploring_rust/refs/heads/main/cargo-projects/asyncing/src/mock-data/books.json";
 
-async fn api_call() -> Result<Vec<BookData>, ApiError> {
-    let response = reqwest::get(BOOKS_URL)
-        .await
-        .map_err(ApiError::RequestError)?;
-
-    let data = response
-        .json::<EndpointResponse>()
-        .await
-        .map_err(ApiError::JsonParserError)?;
-
-    Ok(data.data)
+#[derive(Deserialize, Serialize)]
+enum ResponseStatus {
+    Success,
+    Fail,
+    Error,
 }
+
+impl fmt::Display for ResponseStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ResponseStatus::Success => write!(f, "success"),
+            ResponseStatus::Fail => write!(f, "fail"),
+            ResponseStatus::Error => write!(f, "error"),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct OkResponse {
+    data: Vec<BookData>,
+    status: String,
+}
+
+#[debug_handler]
+async fn get_books() -> (StatusCode, Response) {
+    let response = reqwest::get(BOOKS_URL).await;
+
+    let Ok(res) = response else {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(OkResponse {
+                status: ResponseStatus::Error.to_string(),
+                data: vec![],
+            })
+            .into_response(),
+            // Body::new(Json(OkResponse {
+            //     data: [],
+            //     status: ResponseStatus::Fail.to_string(),
+            // }))
+            // .into_response(),
+        );
+        // return (StatusCode::BAD_REQUEST, Body::empty()).into_response();
+    };
+
+    let r = res.json::<EndpointResponse>().await.unwrap();
+    (
+        StatusCode::OK,
+        Json(OkResponse {
+            status: ResponseStatus::Success.to_string(),
+            data: r.data,
+        })
+        .into_response(),
+    )
+}
+
+// let data = response
+//     .json::<EndpointResponse>()
+//     .await
+//     .map_err(ApiError::JsonParserError)?;
+
+// Ok(data.data)
 
 #[derive(Debug, Serialize, Deserialize)]
 struct BookData {
@@ -37,13 +96,18 @@ struct EndpointResponse {
     data: Vec<BookData>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct EndpointError {}
+
 async fn root() -> String {
     "Hello, world!\n".to_owned()
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/books", get(get_books));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
