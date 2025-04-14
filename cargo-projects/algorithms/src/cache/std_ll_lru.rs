2a3a65cef7b4a021966c3cef541d493b3b11c995
David Chubabriya <david.chubabriya@gmail.com>
2025-04-14 22:29:54 +0100

    feat(algorithms): swap linkedlist for vecdeque

--- cargo-projects/algorithms/src/cache/std_ll_lru.rs
+++ cargo-projects/algorithms/src/cache/std_ll_lru.rs
@@ -1,19 +1,21 @@
 use std::{
     borrow::Borrow,
-    collections::{HashMap, LinkedList},
+    collections::{HashMap, LinkedList, VecDeque},
     hash::Hash,
+    rc::Rc,
 };
 
 struct LRUCache<T> {
-    data: LinkedList<T>,
-    map: HashMap<T, Option<T>>,
+    // data: LinkedList<Rc<T>>,
+    data: VecDeque<T>,
+    map: HashMap<T, usize>,
     capacity: usize,
 }
 
 impl<T: Eq + Hash + Clone> LRUCache<T> {
     fn new(capacity: usize) -> Self {
         Self {
-            data: LinkedList::new(),
+            data: VecDeque::new(),
             map: HashMap::new(),
             capacity,
         }
@@ -27,7 +29,8 @@ impl<T: Eq + Hash + Clone> LRUCache<T> {
         // to the head of the linked list since
         // it's last accessed element. For that we need a reference to Node.
         let location = self.map.get(val);
-        location?.as_ref()
+        let loc = location?;
+        self.data.get(*loc)
     }
 
     fn put(&mut self, value: T) {
@@ -37,8 +40,8 @@ impl<T: Eq + Hash + Clone> LRUCache<T> {
         }
 
         self.data.push_back(value.clone());
-        let node = self.data.back();
-        self.map.insert(value, node.cloned());
+        let item_position = self.data.len() - 1;
+        self.map.insert(value, item_position);
     }
 }
 
@@ -58,12 +61,17 @@ mod std_ll_lru_tests {
     fn works_2() {
         let mut l = LRUCache::new(1);
         let actual = l.get(&"hello");
-
         assert_eq!(actual, None);
 
+        // First element inserted
         l.put("hello");
-        let g = l.get(&"hello").cloned();
+        let item = l.get(&"hello").cloned();
+        assert_eq!(item, Some("hello"));
 
-        assert_eq!(g, Some(&"hello").map(|v| &**v));
+        // Removes previous element since capacity is 1
+        l.put("hi");
+        let item = l.get(&"hi").cloned();
+        assert!(l.data.len() == 1);
+        assert_eq!(item, Some("hi"));
     }
 }
