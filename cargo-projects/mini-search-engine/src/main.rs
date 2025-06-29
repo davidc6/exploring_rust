7babd28b4566d4d0976fd51de2397ce54f49f117
David Chubabriya <david.chubabriya@gmail.com>
2025-06-29 10:03:13 +0100

    feat(mini-search-engine): add indexing logic

--- cargo-projects/mini-search-engine/src/main.rs
+++ cargo-projects/mini-search-engine/src/main.rs
@@ -1,13 +1,25 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::sync::mpsc::{channel, Receiver};
-use std::thread;
+use std::thread::{self, JoinHandle};
+
+#[derive(Default)]
+struct InvertedIndex {
+    index: HashMap<String, HashMap<String, usize>>,
+}
+
+impl InvertedIndex {
+    fn from_value(id: usize, value: String) -> Self {
+        InvertedIndex::default()
+    }
+}
 
 /// Attempts to read all the files in a given directory and
 /// send the content of each file on the channel.
 ///
 /// Exits if a file cannot be read. If all operations are successful,
 /// returns a tuple - a Receiver and JoinHandle.
-fn file_reader(files: Vec<String>) -> (Receiver<String>, thread::JoinHandle<std::io::Result<()>>) {
+fn file_reader(files: Vec<String>) -> (Receiver<String>, JoinHandle<std::io::Result<()>>) {
     let (tx, rx) = channel();
 
     let handle = thread::spawn(move || {
@@ -20,6 +32,22 @@ fn file_reader(files: Vec<String>) -> (Receiver<String>, thread::JoinHandle<std:
         }
 
         Ok(())
+    });
+
+    (rx, handle)
+}
+
+fn file_indexing(content: Receiver<String>) -> (Receiver<InvertedIndex>, JoinHandle<()>) {
+    let (tx, rx) = channel();
+
+    let handle = thread::spawn(move || {
+        for (id, value) in content.into_iter().enumerate() {
+            let index = InvertedIndex::from_value(id, value);
+
+            let Ok(_) = tx.send(index) else {
+                break;
+            };
+        }
     });
 
     (rx, handle)
