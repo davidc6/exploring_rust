232c95d27768489eeb121ec1c141ec480e17bbd9
David Chubabriya <david.chubabriya@gmail.com>
2025-06-29 19:53:24 +0100

    feat(mini-search-engine): add initial content splitter implementation

--- cargo-projects/mini-search-engine/src/main.rs
+++ cargo-projects/mini-search-engine/src/main.rs
@@ -7,18 +7,56 @@ use std::sync::mpsc::{channel, Receiver};
 use std::thread::{self, JoinHandle};
 
 /// ID: { word_1: 1, word_2: 4, word_3: 2 }
+///
+/// [word_1]: { id_1: [1, 2], id_2: [4. 5] }
+/// [word_2]: { id_1: [2, 3, 7], id_3: [6, 9] }
+///
+/// 1: { a: 1, b: 2, c: 4 }
+/// 2: { c: 1, b: 3, f: 1 }
+///
+/// merged
+///
 /// An id is a unique identifier for each document in the corpus
 #[derive(Default, Debug)]
 struct InvertedIndex {
-    index: HashMap<String, HashMap<String, usize>>,
+    index: HashMap<String, HashMap<Filename, Vec<usize>>>,
 }
 
 impl InvertedIndex {
     fn from_value(id: Filename, value: Content) -> Self {
-        InvertedIndex::default()
+        let mut index = InvertedIndex::default();
+
+        let lines = value.0.lines();
+        let mut count = 0;
+
+        for line in lines {
+            let split = line.split(' ');
+
+            for word in split {
+                count += 1;
+                index
+                    .index
+                    .entry(word.to_owned())
+                    .and_modify(|v| v.get_mut(&id).unwrap().push(count))
+                    .or_insert_with(|| {
+                        let mut hm = HashMap::new();
+                        hm.insert(Filename(id.0.clone()), vec![count]);
+                        hm
+                    });
+            }
+        }
+
+        index
+    }
+
+    fn merge(&mut self, index: InvertedIndex) {
+        for (key, vals) in index.index {
+            self.index.entry(key).or_insert_with(|| vals);
+        }
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Filename(String);
 struct Content(String);
 
@@ -77,12 +115,30 @@ fn file_indexing(
     (rx, handle)
 }
 
+fn merge_indices(indices: Receiver<InvertedIndex>) -> (Receiver<InvertedIndex>, JoinHandle<()>) {
+    let (tx, rx) = channel();
+
+    let handle = thread::spawn(move || {
+        let mut indices_merged = InvertedIndex::default();
+
+        for index in indices.into_iter() {
+            indices_merged.merge(index);
+        }
+
+        println!("INDEX {:?}", indices_merged);
+    });
+
+    (rx, handle)
+}
+
 fn execute(files: Vec<PathBuf>) -> io::Result<()> {
     let (reader, reader_join) = file_reader(files);
     let (indexer, indexer_join) = file_indexing(reader);
+    let (merger, merger_join) = merge_indices(indexer);
 
     let reader_1 = reader_join.join().unwrap();
     indexer_join.join().unwrap();
+    merger_join.join().unwrap();
 
     reader_1?;
 
