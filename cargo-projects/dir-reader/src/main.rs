781fe1c4aa5029376ca99802304fbc4604957c53
David Chubabriya <david.chubabriya@gmail.com>
2025-12-12 21:50:27 +0000

    feat(dir-reader): remove obsolete code

--- cargo-projects/dir-reader/src/main.rs
+++ cargo-projects/dir-reader/src/main.rs
@@ -1,30 +1,11 @@
-use std::{env, error::Error, fs::{read_dir, ReadDir}, io::Result, iter, path::Path};
+use std::{env, fs::{read_dir, ReadDir}, io::Result, iter, path::Path};
 
-// fn traverse_file_tree(space_count: u8, path: &ReadDir) {
-//     for (index, entry) in read_dir(path).into_iter().enumerate() {
-//         // let dir_entry = entry
-//         let path = entry;
-// 
-//         let full_str = iter::repeat_n(" ", space_count.into());
-//         let f: String = full_str.into_iter().collect();
-// 
-//         if path.read_dir().unwrap().is_dir() {
-//             traverse_file_tree(space_count + 2, &path);
-//         }
-// 
-//         let fs = format!("{}-- {:?}", f, entry);
-//         println!("{}", fs);
-//     }
-// }
-
-fn trav(current_directory: Result<ReadDir>, depth: usize) {
+fn traverse_file_tree(current_directory: Result<ReadDir>, depth: usize) {
     let current_directory = current_directory.unwrap();
     for dir_entry in current_directory {
         let Ok(dir_entry) = dir_entry else {
             panic!("Directory entry is invalid");
         };
-
-        // let path = dir_entry.path();
 
         let path = dir_entry.path();
         let last_entry = path.iter().last().unwrap();
@@ -40,17 +21,9 @@ fn trav(current_directory: Result<ReadDir>, depth: usize) {
             panic!("Current path incorrect");
         };
 
-        // println!("{:?}", path);
-
         if path.is_dir() {
-            // let w = format!("{}-- {}", l.join(""), p);
             let w = read_dir(path.as_path());
-
-            // println!("  {}", last_entry_unwrapped);
-            trav(w, depth + 2);
-        } else {
-            // let h = format!("{}-- {}", l.join(""), p);
-            // println!("{}", last_entry_unwrapped);
+            traverse_file_tree(w, depth + 4);
         }
     }
 }
@@ -61,6 +34,6 @@ fn main() {
     let current_directory = Path::new(".").read_dir();
 
     // traverse_file_tree(0, &a.unwrap());
-    trav(current_directory, 2);
+    traverse_file_tree(current_directory, 0);
 }
 
