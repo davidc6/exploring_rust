7b110a2f35b8863898193c35608589ab7c6c2ec3
David Chubabriya <david.chubabriya@gmail.com>
2025-12-13 19:22:29 +0000

    feat(dir-reader): handle unwraps

--- cargo-projects/dir-reader/src/main.rs
+++ cargo-projects/dir-reader/src/main.rs
@@ -1,25 +1,28 @@
 use std::{env, fs::{read_dir, ReadDir}, io::Result, iter, path::Path};
 
 fn traverse_file_tree(current_directory: Result<ReadDir>, depth: usize) {
-    let current_directory = current_directory.unwrap();
+    let Ok(current_directory) = current_directory else {
+        panic!("Cannot read current directory");
+    };
+
     for dir_entry in current_directory {
         let Ok(dir_entry) = dir_entry else {
             panic!("Directory entry is invalid");
         };
 
         let path = dir_entry.path();
-        let last_entry = path.iter().last().unwrap();
-        let last_entry_unwrapped = last_entry.to_str().unwrap();
-
-        let l = iter::repeat_n(" ", depth);
-        let l: Vec<_> = l.collect();
-        let w = format!("{}{}", l.join(""), last_entry_unwrapped);
-
-        println!("{}", w);
-
-        let Ok(current_dir) = env::current_dir() else {
-            panic!("Current path incorrect");
+        let Some(last_entry) = path.iter().last() else {
+            panic!("Cannot get file name");
         };
+
+        let Some(last_entry_unwrapped) = last_entry.to_str() else {
+            panic!("Cannot cast entry into a string");
+        };
+
+        let spacing = iter::repeat_n(" ", depth).collect::<Vec<_>>();
+        let value = format!("{}{}", spacing.join(""), last_entry_unwrapped);
+
+        println!("{}", value);
 
         if path.is_dir() {
             let w = read_dir(path.as_path());
