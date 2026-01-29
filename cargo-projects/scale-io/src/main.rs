ac9eb8ea532dab101778d18704d5882d53c8c987
David Chubabriya <david.chubabriya@gmail.com>
2026-01-29 21:03:29 +0000

    docs(async): add comments to Poll and calling code example to main

--- cargo-projects/scale-io/src/main.rs
+++ cargo-projects/scale-io/src/main.rs
@@ -1,6 +1,15 @@
+// use crate::poll::Poll;
+
 mod ffi;
 mod poll;
 
 fn main() {
     println!("Hello, world!");
+
+    // TODO
+    // let event_queue = Poll::new();
+    // let id = 0;
+    // event_queue.registy().register(&tcp_stream, id)
+    // let mut events = Vec::with_capacity(1);
+    // event_queue.poll(&mut events, None);
 }
--- cargo-projects/scale-io/src/poll.rs
+++ cargo-projects/scale-io/src/poll.rs
@@ -1,31 +1,40 @@
-use std::net::TcpStream;
+use std::{io::Result, net::TcpStream};
 use crate::ffi;
+
+/// This is layer over epoll.
+///
+/// epoll (event poll) is referred to as an I/O event notification facility.
+/// It is Linux-specific but it is not POSIX.
+///
+/// It is used to monitor multiple file descriptors to check if they are ready for I/O.
 
 type Events = Vec<ffi::Event>;
 
-/// Poll represents the event queue.
+/// Poll represents the Event Queue.
 pub struct Poll {
     registry: Registry
 }
 
 impl Poll {
     /// Creates an event queue.
-    fn new() -> Self {
+    pub fn new() -> Self {
         todo!()
     }
 
-    /// Returns a registry to register interest about events.
+    /// Returns a reference to a registry . It is used as a handle to register interest about new 
+    /// events.
     fn registry(&self) -> &Registry {
         &self.registry
     }
 
-    /// Blocks the thread while polling 
-    /// until the event is ready or times out.
+    /// Blocks the thread while polling until the event is ready or times out.
     fn poll(&mut self, events: &mut Events, timeout: Option<i32>) {
         todo!()
     }
 }
 
+/// Registry allows us to register interest in new events.
+/// This will indicate what kind of events we want Poll (our event queue) to keep track of.
 pub struct Registry {
     raw_file_descriptor: i32
 }
