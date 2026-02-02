b1a0d2f2123e4210e81bb4e55b8db31403c980a3
David Chubabriya <david.chubabriya@gmail.com>
2026-02-02 22:19:13 +0000

    feat(async-one): add initial block_on implementation

--- async/async-one/src/main.rs
+++ async/async-one/src/main.rs
@@ -1,5 +1,18 @@
 use std::future::Future;
 use tokio::runtime::Runtime;
+use reqwest;
+
+struct Response(reqwest::Response);
+
+impl Response {
+    pub async fn text(self) -> String {
+        self.0.text().await.unwrap()
+    }
+}
+
+async fn get(url: &str) -> Response {
+    Response(reqwest::get(url).await.unwrap())
+}
 
 /// Runs a single future to completion.
 ///
@@ -10,6 +23,14 @@ fn block_on<F: Future>(future: F) -> F::Output {
     rt.block_on(future)
 }
 
+async fn get_page(url: &str) -> Option<String> {
+    let res = get("https://jsonplaceholder.typicode.com/todos/1").await.text().await;
+    Some(res)
+}
+
 fn main() {
-    println!("Hello, world!");
+    block_on(async {
+        let r =  get_page("cc").await.unwrap();
+        print!("{r}");
+    });
 }
